6ef5e5fc772edad7bfc169bc4eeec03d80ac0862
fengyong <fkworld@foxmail.com>
2018-07-26 16:36:11 +0800

    新增res的统一管理方式

---
+++ assets/Script/framework/ResManager.js
@@ -0,0 +1,102 @@
+const { ccclass, property } = cc._decorator
+/** 配置参数 */
+const C = {
+    /** panel路径参数 */
+    PATH_PANEL: "panel",
+    /** audio路径参数 */
+    PATH_AUDIO: "audio",
+}
+
+/**
+ * 框架文件：资源管理器
+ * - 针对变动的动态文件
+ * - 资源路径写在模块开头的C中
+ * - 资源路径需要在resource文件夹下
+ * - 脚本需要挂载在尽量靠前的位置
+ * @class
+ */
+@ccclass
+export default class ResManager extends cc.Component {
+
+    onLoad() {
+        // 初始化
+        this.load_count = 0
+        this.total_count = 0
+        /** 是否全部载入完毕 */
+        this.is_load_over = false
+
+        // 初始化存储
+        /** panel数组 */
+        this.array_panel = Array.of()
+        /** audio数组 */
+        this.array_audio = Array.of()
+
+        // 载入
+        this.load_res(C.PATH_AUDIO, cc.AudioClip, this.array_audio)
+        this.load_res(C.PATH_PANEL, cc.Prefab, this.array_panel)
+
+        // 保存实例
+        ResManager.instance = this
+    }
+
+    /** 总计数
+     * @type {number}
+     */
+    get total_count() { return this._total_count }
+    set total_count(count) { this._total_count = count }
+
+    /** 载入计数
+     * @type {number}
+     */
+    get load_count() { return this._load_count }
+    set load_count(count) { this._load_count = count }
+
+    /** 脚本实例 
+     * @type {ResManager}*/
+    static get ins() { return ResManager.instance }
+
+    /**
+     * 载入资源
+     * @param {string} path 路径
+     * @param {typeof cc.Asset} type 类型
+     * @param {[]} array 资源数组
+     */
+    load_res(path, type, array) {
+        /** 是否已经计数 */
+        let is_count = false
+        cc.loader.loadResDir(
+            // 路径
+            path,
+            // 格式
+            type,
+            // 载入全部资源后的回调函数
+            (err, res) => {
+                // 载入失败
+                if (err) {
+                    cc.error("载入资源失败，path&type=", path, type.toString(), "err=", err)
+                    this.check_load_over()
+                    return
+                }
+                // 写入数据
+                // 其中两个count并非是在载入过程中写入，而是在最后统计过程中写入；原因是载入过程机制不明
+                this.total_count += res.length
+                for (let r of res) {
+                    array.push(r)
+                    this.load_count += 1
+                }
+                // 载入成功
+                cc.info("载入资源成功，path=", path)
+                this.check_load_over()
+            }
+        )
+    }
+
+    /** 检查是否载入完毕 */
+    check_load_over() {
+        if (this.load_count >= this.total_count) {
+            this.is_load_over = true
+        } else {
+            this.is_load_over = false
+        }
+    }
+}
--- assets/Script/framework/UIPanel.js
+++ assets/Script/framework/UIPanel.js
@@ -1,18 +1,27 @@
+/** 配置参数 */
+const C = {
+    /** 默认动作时间 */
+    DEFAULT_TIME: 0.2,
+}
+
 /**
  * 框架文件：游戏窗口的方法封装类
+ * @author fengyong
+ * @class
  */
 export default class UIPanel {
 
     /**
-     * 构造函数
-     * @param {cc.Node} node 窗口节点
+     * 构造函数：传入窗口的根节点
+     * @param {cc.Node} node 窗口根节点
      */
     constructor(node) {
         this.node = node
     }
 
-    /** 窗口节点
-     * @returns {cc.Node}
+    /** 
+     * 窗口节点
+     * @type {cc.Node}
      */
     get node() { return this._node }
     set node(node) { this._node = node }
@@ -22,12 +31,12 @@ export default class UIPanel {
      * - 为了统一窗口显示方式，在外部调用时尽量只通过这个方法
      */
     show() {
-        this.panel_node.stopAllActions()
+        this.node.stopAllActions()
         // 特别要注意这里
         // 可能会出现多次调用hide()方法，但是当第一个hide()方法执行成功后，节点隐藏，其余的hide()方法为静默状态
         // 下次show()的时候，则会继续执行静默的hide()方法，导致界面异常隐藏掉
 
-        // this.show_with_nothing()
+        // 设置默认的显示方式
         this.show_with_scale()
     }
 
@@ -36,39 +45,57 @@ export default class UIPanel {
      * - 为了统一窗口隐藏方式，在外部调用时尽量只通过这个方法
      */
     hide() {
-        // this.hide_with_nothing()
         this.hide_with_scale()
     }
 
-    // 具体的show和hide实现方法
+    //////////
+    // 以下方法为具体show和hide实现方法
+    //////////
 
     /** 显示窗口：没有任何动画 */
     show_with_nothing() {
-        this.panel_node.active = true
+        this.node.active = true
     }
 
     /** 隐藏窗口：没有任何动画 */
     hide_with_nothing() {
-        this.panel_node.active = false
+        this.node.active = false
     }
 
-    /** 显示窗口：放大缩小动画 */
+    /** 
+     * 显示窗口：放大缩小动画
+     * @returns {number} action time
+     */
     show_with_scale() {
-        this.panel_node.scale = 0
+        let time = C.DEFAULT_TIME
+        let action = cc.scaleTo(time, 1)
+
+        // 前摇
+        // 特别注意：只有当node.active为true时，才可以执行动作；因此在前摇结束后需要保证node.active为true
+        this.node.scale = 0
         this.show_with_nothing()
-        let action = cc.scaleTo(0.2, 1)
-        this.panel_node.runAction(action)
+        // 动作
+        this.node.runAction(action)
+
+        return time
     }
 
-    /** 隐藏窗口：放大缩小动画 */
+    /** 隐藏窗口：放大缩小动画
+     * @returns {number} action time
+     */
     hide_with_scale() {
-        this.panel_node.scale = 1
+        let time = C.DEFAULT_TIME
         let action = cc.sequence(
-            cc.scaleTo(0.2, 0),
+            cc.scaleTo(time, 0),
             cc.callFunc(() => {
                 this.hide_with_nothing()
             })
         )
-        this.panel_node.runAction(action)
+
+        // 前摇
+        // 动作
+        this.node.runAction(action)
+
+        return time
     }
 }
