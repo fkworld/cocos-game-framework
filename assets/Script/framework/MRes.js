ff2e7eccdffb848b72d4371f9885fea26c8150d7
fengyong <fkworld@foxmail.com>
2018-07-27 19:57:10 +0800

    使用promise封装（还有一些bug）

--- assets/Script/framework/MRes.js
+++ assets/Script/framework/MRes.js
@@ -31,9 +31,8 @@ export default class MRes extends cc.Component {
         /** audio数组 */
         this.array_audio = Array.of()
 
-        // 载入
-        this.load_res(C.PATH_AUDIO, cc.AudioClip, this.array_audio)
-        this.load_res(C.PATH_PANEL, cc.Prefab, this.array_panel)
+        // 链式加载
+        this.load()
 
         // 保存实例
         MRes.instance = this
@@ -55,6 +54,13 @@ export default class MRes extends cc.Component {
     get load_count() { return this._load_count }
     set load_count(count) { this._load_count = count }
 
+    async load() {
+        let ret = null
+        ret = await this.load_res(C.PATH_PANEL, cc.Prefab, this.array_panel)
+        ret = await this.load_res(C.PATH_AUDIO, cc.AudioClip, this.array_audio)
+        return ret
+    }
+
     /**
      * 载入资源
      * @param {string} path 路径
@@ -62,41 +68,32 @@ export default class MRes extends cc.Component {
      * @param {[]} array 资源数组
      */
     load_res(path, type, array) {
-        /** 是否已经计数 */
-        let is_count = false
-        cc.loader.loadResDir(
-            // 路径
-            path,
-            // 格式
-            type,
-            // 载入全部资源后的回调函数
-            (err, res) => {
-                // 载入失败
-                if (err) {
-                    cc.error("载入资源失败，path&type=", path, type.toString(), "err=", err)
-                    this.check_load_over()
-                    return
+        return new Promise((resolve, reject) => {
+            cc.loader.loadResDir(
+                // 路径
+                path,
+                // 格式
+                type,
+                // 载入全部资源后的回调函数
+                (err, res) => {
+                    // 载入失败
+                    if (err) {
+                        cc.error("载入资源失败，path&type=", path, type.toString(), "err=", err)
+                        reject("aa")
+                        return
+                    }
+                    // 写入数据
+                    // 其中两个count并非是在载入过程中写入，而是在最后统计过程中写入；原因是载入过程机制不明
+                    this.total_count += res.length
+                    for (let r of res) {
+                        array.push(r)
+                        this.load_count += 1
+                    }
+                    // 载入成功
+                    cc.log("载入资源成功，path=", path)
+                    resolve("aaa")
                 }
-                // 写入数据
-                // 其中两个count并非是在载入过程中写入，而是在最后统计过程中写入；原因是载入过程机制不明
-                this.total_count += res.length
-                for (let r of res) {
-                    array.push(r)
-                    this.load_count += 1
-                }
-                // 载入成功
-                cc.log("载入资源成功，path=", path)
-                this.check_load_over()
-            }
-        )
-    }
-
-    /** 检查是否载入完毕 */
-    check_load_over() {
-        if (this.load_count >= this.total_count) {
-            this.is_load_over = true
-        } else {
-            this.is_load_over = false
-        }
+            )
+        })
     }
 }
