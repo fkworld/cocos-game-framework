a24bdc1eacc8f3e5cb037b4e2e30c771cd4b8d1d
fengyong <fkworld@foxmail.com>
2018-08-06 18:31:01 +0800

    完善Global，新增1个相同概率随机取方法，新增封装的设置node的click事件

--- assets/Script/framework/Global.js
+++ assets/Script/framework/Global.js
@@ -28,6 +28,16 @@ class Global {
     }
 
     /**
+     * 从数组中获取一个随机值（概率相等）
+     * @param {[]} array 
+     */
+    random_item_form_array(array) {
+        if (array.length === 0) { return }
+        let rn = this.random_int(0, array.length)
+        return array[rn]
+    }
+
+    /**
      * 将此节点设置为btn_like节点
      * - 包含：放大缩小效果，按钮点击音效
      * - 特别注意：由于引擎load顺序的关系，尽量将audio的节点放在前面，UI节点放在后面，否则会有一些莫名其妙的bug
@@ -45,6 +55,19 @@ class Global {
         // node.on(cc.Node.EventType.TOUCH_END, () => {
 
         // }, node)
+    }
+
+    /**
+     * 设置node节点的标准点击事件
+     * @param {cc.Node} node 节点
+     * @param {boolean} is_btn_like 是否有点击效果
+     * @param {Function} f 点击处理函数
+     */
+    set_event(node, is_btn_like, f) {
+        node.on(cc.Node.EventType.TOUCH_END, f)
+        if (is_btn_like) {
+            this.set_as_btn_like(node)
+        }
     }
 
 }
