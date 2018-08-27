8f89d218c6312a5578854aed74a21a200f538b22
fengyong <fkworld@foxmail.com>
2018-08-27 19:27:58 +0800

    清理MRes.js

--- assets/Script/framework/MRes.js
+++ assets/Script/framework/MRes.js
@@ -24,14 +24,10 @@ export default class MRes extends cc.Component {
     static ins
 
     onLoad() {
-        // 初始化
-        this.load_count = 0
-        this.total_count = 0
-
         // 初始化存储
-        /** @type {[cc.Prefab]} panel数组 */
+        /** @type {Array<cc.Prefab>} panel数组 */
         this.array_panel = Array.of()
-        /** audio数组 */
+        /** @type {Array<cc.AudioClip>} audio数组 */
         this.array_audio = Array.of()
 
         // 链式加载
@@ -42,15 +38,7 @@ export default class MRes extends cc.Component {
         MRes.ins = this
     }
 
-    /** @type {number} 总计数 */
-    get total_count() { return this._total_count }
-    set total_count(count) { this._total_count = count }
-
-    /** @type {number} 载入计数 */
-    get load_count() { return this._load_count }
-    set load_count(count) { this._load_count = count }
-
-    /** 
+    /**
      * 资源加载链
      * @returns {Promise}
      */
@@ -65,35 +53,23 @@ export default class MRes extends cc.Component {
      * 载入资源
      * @param {string} path 路径
      * @param {typeof cc.Asset} type 类型
-     * @param {[]} array 资源数组
+     * @param {Array} array 资源数组
      */
     load_res(path, type, array) {
         return new Promise((resolve, reject) => {
-            cc.loader.loadResDir(
-                // 路径
-                path,
-                // 格式
-                type,
-                // 载入全部资源后的回调函数
-                (err, res) => {
-                    // 载入失败
-                    if (err) {
-                        cc.log("载入资源失败，path&type=", path, type.toString(), "err=", err)
-                        reject()
-                        return
-                    }
-                    // 写入数据
-                    // 其中两个count并非是在载入过程中写入，而是在最后统计过程中写入；原因是载入过程机制不明
-                    this.total_count += res.length
-                    for (let r of res) {
-                        array.push(r)
-                        this.load_count += 1
-                    }
-                    // 载入成功
-                    cc.log("资源载入成功，path=", path)
-                    resolve()
+            cc.loader.loadResDir(path, type, (err, res) => {
+                // 载入失败
+                if (err) {
+                    cc.log("载入资源失败，path&type=", path, type.toString(), "err=", err)
+                    reject()
+                    return
                 }
-            )
+                // 写入数据
+                for (let r of res) { array.push(r) }
+                // 载入成功
+                cc.log("资源载入成功，path=", path)
+                resolve()
+            })
         })
     }
 }
