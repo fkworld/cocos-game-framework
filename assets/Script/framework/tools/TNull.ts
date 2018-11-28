85f05926e4569ba82ed51b4ceb967aa645ab0611
fengyong <fkworld@foxmail.com>
2018-11-28 13:06:40 +0800

    添加一个空组件脚本工具

---
+++ assets/Script/framework/tools/TNull.ts
@@ -0,0 +1,25 @@
+// Learn TypeScript:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/typescript.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const { ccclass, property } = cc._decorator;
+
+/**
+ * [framework-T] 空组件
+ * - 解决cc.Node不支持的一些cc.Component方法，例如schedule()
+ */
+@ccclass
+export default class TNull extends cc.Component {
+
+    static get(node: cc.Node) {
+        if (node.getComponent(TNull)) { return node.getComponent(TNull) }
+        return node.addComponent(TNull)
+    }
+
+}
