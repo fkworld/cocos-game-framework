ccdd4e862d41e5b3d16b1debb4a9b01a8dc6ff83
fengyong <fkworld@foxmail.com>
2018-12-12 19:14:21 +0800

    新增一个资源排序算法

--- assets/Script/framework/MRes.ts
+++ assets/Script/framework/MRes.ts
@@ -11,6 +11,7 @@ Object.freeze(C)
  * - 资源路径写在模块开头的C中
  * - 资源路径需要在resource文件夹下
  * - 脚本需要挂载在尽量靠前的位置
+ * - [注意天坑] 编辑器中的载入顺序与打包之后的载入顺序不同（不同的打包平台顺序也不同），因此在载入完成后需要对数组进行排序。排序算法是依据资源的name属性，选取资源name属性的第1个字符转换为数字后进行排序
  */
 @ccclass
 export class MRes extends cc.Component {
@@ -41,7 +42,7 @@ export class MRes extends cc.Component {
             cc.loader.loadRes(path, type, (err, res) => {
                 // 载入失败
                 if (err) {
-                    cc.error(`[MRes] resource load fail,path=${path},type=${type},error=${err}`)
+                    cc.error(`[${MRes.name}] resource load fail,path=${path},type=${type},error=${err}`)
                     reject()
                     return
                 }
@@ -65,17 +66,22 @@ export class MRes extends cc.Component {
             cc.loader.loadResDir(path, type, (err, res) => {
                 // 载入失败
                 if (err) {
-                    cc.error(`[MRes] resource load fail,path=${path},type=${type},error=${err}`)
+                    cc.error(`[${MRes.name}] resource load fail,path=${path},type=${type},error=${err}`)
                     reject()
                     return
                 }
                 // 载入成功
-                cc.warn(`[MRes] resource load success,length=${res.length}`)
-                if (res.length === 0) { cc.warn(`[MRes] resource length=0,please check again`) }
+                cc.warn(`[${MRes.name}] resource load success,length=${res.length}`)
+                if (res.length === 0) { cc.warn(`[${MRes.name}] resource length=0,please check again`) }
                 // 写入数据
                 resolve(res)
             })
         })
+    }
+
+    /** 资源排序 */
+    static sort(array: any[]) {
+        array.sort((a, b) => { return Number.parseInt(a.name[0]) - Number.parseInt(b.name[0]) })
     }
 }
 
