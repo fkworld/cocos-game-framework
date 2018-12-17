6deb2ea3829a00cbd898b322d32457c2fb181d21
fengyong <fkworld@foxmail.com>
2018-12-17 14:53:58 +0800

    优化资源管理，添加一个资源排序方法

--- assets/Script/framework/MRes.ts
+++ assets/Script/framework/MRes.ts
@@ -7,9 +7,8 @@ Object.freeze(C)
 
 /**
  * [framework-M] 动态资源管理
- * - 针对变动的动态文件
- * - 资源路径写在模块开头的C中
- * - 资源路径需要在resource文件夹下
+ * - 载入变动的资源文件
+ * - 资源路径需要在resource文件夹下，资源路径写在模块开头的C中
  * - 脚本需要挂载在尽量靠前的位置
  * - [注意天坑] 编辑器中的载入顺序与打包之后的载入顺序不同（不同的打包平台顺序也不同），因此在载入完成后需要对数组进行排序。排序算法是依据资源的name属性，选取资源name属性的第1个字符转换为数字后进行排序
  */
@@ -40,14 +39,14 @@ export class MRes extends cc.Component {
     static async load_res(path: string, type: typeof cc.Asset) {
         return await new Promise((resolve, reject) => {
             cc.loader.loadRes(path, type, (err, res) => {
-                // 载入失败
                 if (err) {
-                    cc.error(`[${MRes.name}] resource load fail,path=${path},type=${type},error=${err}`)
-                    reject()
-                    return
+                    // 载入失败
+                    cc.error(`[${MRes.name}] resource load fail, path=${path}, type=${type}, error=${err}`)
+                    reject(err)
+                } else {
+                    // 载入成功
+                    resolve(res)
                 }
-                // 载入成功
-                resolve(res)
             })
         })
     }
@@ -64,24 +63,25 @@ export class MRes extends cc.Component {
     static async load_res_dir(path: string, type: typeof cc.Asset): Promise<any> {
         return await new Promise((resolve, reject) => {
             cc.loader.loadResDir(path, type, (err, res) => {
-                // 载入失败
                 if (err) {
-                    cc.error(`[${MRes.name}] resource load fail,path=${path},type=${type},error=${err}`)
-                    reject()
-                    return
+                    // 载入失败
+                    cc.error(`[${MRes.name}] resource load fail, path=${path}, type=${type.name}, error=${err}`)
+                    reject(err)
+                } else {
+                    // 载入成功
+                    cc.warn(`[${MRes.name}] resource load success, path=${path}, length=${res.length}, ${res.length === 0 ? 'please check again' : ''}`)
+                    resolve(res)
                 }
-                // 载入成功
-                cc.warn(`[${MRes.name}] resource load success,length=${res.length}`)
-                if (res.length === 0) { cc.warn(`[${MRes.name}] resource length=0,please check again`) }
-                // 写入数据
-                resolve(res)
             })
         })
     }
 
-    /** 资源排序 */
-    static sort(array: any[]) {
-        array.sort((a, b) => { return Number.parseInt(a.name[0]) - Number.parseInt(b.name[0]) })
+    /**
+     * 资源排序：根据name属性的第一个数字
+     * @param a 
+     * @param b 
+     */
+    static sort_by_name(a: any, b: any) {
+        return Number.parseInt(a.name[0]) - Number.parseInt(b.name[0])
     }
 }
-
