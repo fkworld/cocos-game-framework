dc1664d9352d142cceb4c2140b20f65e2a0321ca
fengyong <fkworld@foxmail.com>
2018-12-17 15:14:24 +0800

    优化声音管理器

--- assets/Script/framework/MSound.ts
+++ assets/Script/framework/MSound.ts
@@ -29,12 +29,19 @@ export class MSound extends cc.Component {
     }
 
     /** 与SOUND一一对应的具体声音信息 */
-    array_sound_info: SoundInfo[] = []
+    array_sound_info: ControllerSound[] = []
 
     /** 初始化声音数据 */
     init_data() {
-        this.array_sound_info[SOUND.test_music] = new SoundInfo('test', TYPE.MUSIC, 1)
-        this.array_sound_info[SOUND.test_effect] = new SoundInfo('test', TYPE.EFFECT, 1)
+        this.array_sound_info[SOUND.test_music] = new ControllerSound('test', TYPE.MUSIC, 1)
+        this.array_sound_info[SOUND.test_effect] = new ControllerSound('test', TYPE.EFFECT, 1)
+
+        // 针对music预播放1次
+        for (let csound of this.array_sound_info) {
+            if (csound.type === TYPE.MUSIC) {
+                csound.play().then(() => { csound.stop() })
+            }
+        }
     }
 
     /** 初始化本地存储 */
@@ -44,7 +51,14 @@ export class MSound extends cc.Component {
     static get_sound_switch() { return L.sound === `${true}` }
 
     /** 设置声音开关（直接反向） */
-    static set_sound_switch() { L.sound = L.sound === `${true}` ? false : true }
+    static set_sound_switch() {
+        L.sound = L.sound === `${true}` ? false : true
+        if (MSound.get_sound_switch()) {
+            MSound.resume_all()
+        } else {
+            MSound.pause_all()
+        }
+    }
 
     /** 声音的具体区分名称 */
     static get SOUND() { return SOUND }
@@ -72,23 +86,8 @@ export class MSound extends cc.Component {
 
 }
 
-/** 声音信息类 */
-class SoundInfo {
-
-    /** 对应的资源url */
-    url: string
-
-    /** 声音类型 */
-    type: TYPE
-
-    /** 声音音量大小 */
-    volume: number
-
-    /** 声音的cc.AudioClip资源 */
-    audio_clip: cc.AudioClip
-
-    /** 如果声音播放了，其id */
-    id: number
+/** 声音控制器类 */
+class ControllerSound {
 
     constructor(url: string, type: TYPE, volume: number, audio_clip: cc.AudioClip = undefined, id: number = undefined) {
         this.url = url
@@ -98,36 +97,40 @@ class SoundInfo {
         this.id = id
     }
 
+    /** 对应的资源url */
+    url: string
+    /** 声音类型 */
+    type: TYPE
+    /** 声音音量大小 */
+    volume: number
+    /** 声音的cc.AudioClip资源 */
+    audio_clip: cc.AudioClip
+    /** 如果声音播放了，其id */
+    id: number
+
     /** 播放 */
     async play() {
         if (this.audio_clip === undefined) {
             this.audio_clip = await MRes.load_res(`${C.PATH}/${this.url}`, cc.AudioClip)
         }
-        switch (this.type) {
-            case TYPE.MUSIC:
-                if (this.id === undefined) {
-                    this.id = cc.audioEngine.play(this.audio_clip, true, this.volume)
-                } else {
-                    cc.audioEngine.resume(this.id)
-                }
-                break;
-            case TYPE.EFFECT:
-                cc.audioEngine.play(this.audio_clip, false, this.volume)
-                break;
-            default: break;
+        if (this.type === TYPE.MUSIC) {
+            if (this.id === undefined) {
+                this.id = cc.audioEngine.play(this.audio_clip, true, this.volume)
+            } else {
+                cc.audioEngine.resume(this.id)
+            }
+        } else {
+            cc.audioEngine.play(this.audio_clip, false, this.volume)
         }
     }
 
     /** 停止 */
     stop() {
-        switch (this.type) {
-            case TYPE.MUSIC:
-                if (this.id === undefined) { return }
-                cc.audioEngine.pause(this.id)
-                break;
-            case TYPE.EFFECT:
-                break;
-            default: break;
+        if (this.type === TYPE.MUSIC) {
+            if (this.id === undefined) { return }
+            cc.audioEngine.pause(this.id)
+        } else {
+            // 
         }
     }
 }
