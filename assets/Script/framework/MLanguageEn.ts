b3c51731df7832d0a3cb8d9b30ea40e5c620f239
fengyong <fkworld@foxmail.com>
2019-01-31 16:36:28 +0800

    修改首页展示图内容，新增游戏制作人信息显示，已经对应的多语言配置

--- assets/Script/framework/MLanguageEn.ts
+++ assets/Script/framework/MLanguageEn.ts
@@ -2,6 +2,7 @@
  * [MLanguage] 英文
  */
 export const MLanguage_en = {
+    'panel_loading_creator': 'Created by skyfoxgame-fengyong',
     'panel_loading_health_game_advice': 'Health Game Advice',
     'panel_loading_log': 'The game is loading...',
 
