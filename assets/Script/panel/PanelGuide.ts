fca901d91033c03c0d277ee860ff08638e5e77a0
fengyong <fkworld@foxmail.com>
2019-04-02 17:42:36 +0800

    修改panel-guide

--- assets/Script/panel/PanelGuide.ts
+++ assets/Script/panel/PanelGuide.ts
@@ -2,95 +2,45 @@ import { MPanel, MPanelImplements } from "../framework/MPanel";
 import { MLog } from "../framework/MLog";
 
 const { ccclass, property } = cc._decorator;
-enum GUIDE_TYPE { test1, test2 }
 
 /**
  * [Panel] Guide+system
- * - guide_type -> guide_info -> guide_panel
  */
 @ccclass
 export class PanelGuide extends cc.Component implements MPanelImplements {
 
     static path = 'PanelGuide'
 
-    static async open(type: GUIDE_TYPE) {
-        if (!GUIDE_TYPE[type]) { MLog.error(`@${PanelGuide.name}: get a non-type, type=${type}`); return }
-        MPanel.open(PanelGuide, ...PanelGuide.get_guide_info(type))
+    static async open(world_position: cc.Vec2, info: string) {
+        MPanel.open(PanelGuide, world_position, info)
     }
 
     static async close() {
         MPanel.close(PanelGuide)
     }
 
-    async on_open(mask_world_position: cc.Vec2, mask_width: number, mask_height: number, info: string) {
-        this.init(mask_world_position, mask_width, mask_height, info)
-        await MPanel.in_fade(this.node)
+    async on_open(world_position: cc.Vec2, info: string) {
+        this.arrow_point.position = this.arrow_point.parent.convertToNodeSpaceAR(world_position)
+        this.label_info.string = info
+        MPanel.in_fade_move(this.arrow_point, 'down')
+        await MPanel.in_move(this.bg_info, 'down')
         cc.director.pause()
     }
 
     async on_close() {
         cc.director.resume()
-        await MPanel.out_fade(this.node)
-    }
-
-    /** 引导的枚举类型 */
-    static get TYPE() { return GUIDE_TYPE }
-
-    /**
-     * 获得guide信息
-     * @param type 
-     */
-    static get_guide_info(type: GUIDE_TYPE) {
-        let world_position: cc.Vec2;
-        let width: number;
-        let height: number;
-        let info: string;
-        switch (type) {
-            case GUIDE_TYPE.test1:
-                world_position = cc.v2(500, 500)
-                width = 100
-                height = 100
-                info = `this is a guide info of ${GUIDE_TYPE[GUIDE_TYPE.test1]}`
-                break;
-            case GUIDE_TYPE.test2:
-                world_position = cc.v2(500, 500)
-                width = 100
-                height = 100
-                info = `this is a guide info of ${GUIDE_TYPE[GUIDE_TYPE.test2]}`
-                break;
-            default:
-                break;
-        }
-        return [world_position, width, height, info]
+        MPanel.out_fade_move(this.arrow_point, 'up')
+        await MPanel.out_move(this.bg_info, 'down')
     }
 
     @property(cc.Node)
-    n_mask: cc.Node = null
+    bg_info: cc.Node = null
+
+    @property(cc.Label)
+    label_info: cc.Label = null
 
     @property(cc.Node)
-    n_info: cc.Node = null
-
-    @property(cc.Widget)
-    mask_bg: cc.Widget = null
-
-    @property(cc.Widget)
-    mask_arrow: cc.Widget = null
-
-    /**
-     * 初始化界面
-     * @param mask_world_position mask的世界坐标
-     * @param mask_width 
-     * @param mask_height 
-     * @param info 
-     */
-    init(mask_world_position: cc.Vec2, mask_width: number, mask_height: number, info: string) {
-        this.n_mask.position = this.n_mask.convertToNodeSpaceAR(mask_world_position)
-        this.n_mask.width = mask_width
-        this.n_mask.height = mask_height
-        this.mask_bg.updateAlignment()
-        this.mask_arrow.updateAlignment()
-        this.n_info.getComponent(cc.Label).string = info
-    }
+    arrow_point: cc.Node = null
 
     /** click event close */
     event_close() {
