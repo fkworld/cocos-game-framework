c23ca1530cc088d68502699194063ccfd8f2510c
fengyong <fkworld@foxmail.com>
2019-04-12 15:57:00 +0800

    修改i18n的数据文件，添加其类型验证

--- assets/script/framework/Mi18n_en.ts
+++ assets/script/framework/Mi18n_en.ts
@@ -2,9 +2,10 @@
  *  [Mi18n] 英文
  */
 export const Mi18n_en = {
-    'panel_loading_creator': 'Created by skyfoxgame-fengyong',
-    'panel_loading_log': 'The game is loading...',
+    "panel_loading_creator": "Created by skyfoxgame-fengyong",
+    "panel_loading_log": "The game is loading...",
 
-    'panel_message_yes': 'YES',
-    'panel_message_no': 'NO',
+    "panel_message_title": "Message",
+    "panel_message_yes": "YES",
+    "panel_message_no": "NO",
 }
--- assets/script/framework/Mi18n_zh.ts
+++ assets/script/framework/Mi18n_zh.ts
@@ -1,6 +1,8 @@
+import { Mi18n_en } from "./Mi18n_en";
+
 /**
  * [Mi18n] 中文 
  */
-export const Mi18n_zh = {
+export const Mi18n_zh: { [key in keyof typeof Mi18n_en]?: string } = {
 
 }
