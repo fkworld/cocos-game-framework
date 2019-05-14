c0e22e49ec6d674b65c35c0cd2c26fe572e3677e
fengyong <fkworld@foxmail.com>
2019-05-14 14:03:57 +0800

    完善msound 新增一个获取声音开关的方法

--- assets/script/framework/MSound.ts
+++ assets/script/framework/MSound.ts
@@ -10,8 +10,8 @@ interface ISound {
     id?: number,            // 声音的播放id,默认为null
 }
 const C = {
-    PATH: 'sound',          // 资源路径
-    DEFAULT_SWITCH: true,   // 默认开关
+    BASE_PATH: "sound",     // 资源路径
+    DEFAULT_SWITCH: true,   // 默认声音开关
     SOUND: {                // 声音类型以及其对应的url
         "bgm-test": "test",
         "btn": "test",
@@ -26,52 +26,60 @@ const C = {
 export class MSound {
 
     /** 初始化本地存储 */
-    static init_local() { L.sound = C.DEFAULT_SWITCH }
+    static init_local() {
+        L.sound = C.DEFAULT_SWITCH
+    }
 
-    static ins: MSound
+    static ins: MSound;
 
     /** 初始化 */
     static init() {
         G.check_ins(MSound)
         MSound.ins = new MSound()
-        MSound.ins.init_data()
+        // 初始化声音
+        MSound.ins.map_sound_ins.set("bgm-test", { url: C.SOUND["bgm-test"], loop: true })
+        MSound.ins.map_sound_ins.set("btn", { url: C.SOUND["btn"] })
+        // check
+        if (MSound.ins.map_sound_ins.size != Object.keys(C.SOUND).length) {
+            MLog.warn("@MSound: sound初始化个数异常")
+        }
     }
 
-    /** 与SOUND一一对应的具体声音信息 */
-    private obj_sound_info: { [key in keyof typeof C.SOUND]?: ISound } = {}
+    /** 声音的实例存储 */
+    private map_sound_ins: Map<keyof typeof C.SOUND, ISound> = new Map()
 
-    /** 初始化声音数据 */
-    private init_data() {
-        this.obj_sound_info["bgm-test"] = { url: C.SOUND["bgm-test"], loop: true }
-        this.obj_sound_info["btn"] = { url: C.SOUND["btn"] }
-        // check-length
-        if (Object.keys(C.SOUND).length != Object.keys(this.obj_sound_info).length) {
-            MLog.warn("sound初始化个数异常")
-        }
+    /** 获取声音开关 */
+    static get_sound_switch(): boolean {
+        return L.sound
     }
 
     /** 设置声音开关(直接反向) */
     static set_sound_switch() {
         L.sound = !L.sound
         if (L.sound) {
-            MSound.resume_all()
+            cc.audioEngine.pauseAll()
         } else {
-            MSound.pause_all()
+            cc.audioEngine.resumeAll()
         }
     }
 
     /** 播放某一个声音:play/resume */
     static async play(sound: keyof typeof C.SOUND) {
         if (!L.sound) { return }
-        let info: ISound = MSound.ins.obj_sound_info[sound]
+        let info = MSound.ins.map_sound_ins.get(sound)
         // 载入audio clip资源
-        if (!info.clip) { info.clip = await G.load_res(`${C.PATH}/${info.url}`, cc.AudioClip) }
-        if (!info.clip) { MLog.error(`@MSound: a not exsit audio clip, url=${info.url}`); return }
+        if (!info.clip) {
+            info.clip = await G.load_res(`${C.BASE_PATH}/${info.url}`, cc.AudioClip)
+        }
+        if (!info.clip) {
+            MLog.error(`@MSound: audio clip no exsit, url=${info.url}`)
+            return
+        }
         if (info.loop) {
             // bgm类型,循环播放,只需要播放1次即可
             switch (cc.audioEngine.getState(info.id)) {
                 case cc.audioEngine.AudioState.ERROR: case cc.audioEngine.AudioState.STOPPED:
-                    MSound.ins.obj_sound_info[sound].id = cc.audioEngine.play(info.clip, info.loop, info.volume)
+                    info.id = cc.audioEngine.play(info.clip, info.loop, info.volume)
                     break;
                 case cc.audioEngine.AudioState.PAUSED:
                     cc.audioEngine.resume(info.id)
@@ -81,13 +89,13 @@ export class MSound {
             }
         } else {
             // 普通音效类型,重复播放,相互独立
-            MSound.ins.obj_sound_info[sound].id = cc.audioEngine.play(info.clip, info.loop, info.volume)
+            info.id = cc.audioEngine.play(info.clip, info.loop, info.volume)
         }
     }
 
     /** 停止某一个声音:stop/pause */
     static stop(sound: keyof typeof C.SOUND) {
-        let info: ISound = MSound.ins.obj_sound_info[sound]
+        let info = MSound.ins.map_sound_ins.get(sound)
         if (info.loop) {
             // bgm类型,pause
             cc.audioEngine.pause(info.id)
@@ -95,16 +103,6 @@ export class MSound {
             // 普通音效类型,stop
             cc.audioEngine.stop(info.id)
         }
-    }
-
-    /** 暂停所有声音(现有),主要用于关闭声音,引导界面等 */
-    static pause_all() {
-        cc.audioEngine.pauseAll()
-    }
-
-    /** 继续所有声音 */
-    static resume_all() {
-        cc.audioEngine.resumeAll()
     }
 
     // 常用声音
