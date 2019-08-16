298a66c3f47a1d5d055d6f65c12eae470ee0e729
fengyong <fkworld@foxmail.com>
2019-08-16 15:24:48 +0800

    修改声音存储 声音播放方式 去掉了声音暂停/恢复逻辑

--- assets/script/framework/FSound.ts
+++ assets/script/framework/FSound.ts
@@ -1,22 +1,10 @@
+import { FLocal } from "./FLocal";
 import { FLog } from "./FLog";
 import { G } from "./G";
-import { FLocal } from "./FLocal";
+import { DataSound, SOUND, SOUND_PATH } from "../data/sound";
 
-const C = {
-    PATH: "sound",      // 资源路径
-    SOUND: {            // 声音极其对应的url数据
-        "bgm": "test",
-        "btn": "test",
-    },
-}
-
-/** 声音类型 */
-type TypeSound = keyof typeof C.SOUND;
 /** 声音信息 */
-interface DataSoundInstance {
-    url: string,            // 声音的资源字符串
-    loop?: boolean,         // 是否循环播放,是否为bgm,默认为false
-    volume?: number,        // 音量,默认为1
+interface DataSoundInstance extends DataSound {
     clip?: cc.AudioClip,    // 声音的cc.AudioClip资源,默认为null
     id?: number,            // 声音的播放id,默认为null
 }
@@ -26,90 +14,61 @@ interface DataSoundInstance {
  * - 保存已经载入的声音,cc.AudioClip
  * - 保存已经播放的声音id
  */
-export class FSound {
+export namespace FSound {
 
-    static ins: FSound;
-
-    /** 初始化 */
-    static init() {
-        G.check_ins(FSound)
-        FSound.ins = new FSound()
-        // 初始化声音
-        FSound.ins.map_sound_ins.set("bgm", { url: C.SOUND["bgm"], loop: true })
-        FSound.ins.map_sound_ins.set("btn", { url: C.SOUND["btn"] })
-        // check
-        if (FSound.ins.map_sound_ins.size != Object.keys(C.SOUND).length) {
-            FLog.warn("@FSound: sound初始化个数异常")
-        }
-    }
-
-    /** 声音的实例存储 */
-    private map_sound_ins: Map<keyof typeof C.SOUND, DataSoundInstance> = new Map()
+    let map_sound: Map<string, DataSoundInstance> = new Map()
 
     /** 获取声音开关 */
-    static get_sound_switch(): boolean {
+    export function get_sound_switch(): boolean {
         return FLocal.get("sound") === `${true}`
     }
 
-    /** 设置声音开关(直接反向) */
-    static set_sound_switch() {
-        FLocal.set("sound", `${!this.get_sound_switch()}`)
-        if (this.get_sound_switch()) {
-            cc.audioEngine.pauseAll()
+    /** 反向声音开关 */
+    export function reverse_sound_switch() {
+        FLocal.set("sound", `${!get_sound_switch()}`)
+        if (get_sound_switch()) {
+            play_bgm() // TODO:目前仅支持在开关打开时播放背景音乐
         } else {
-            cc.audioEngine.resumeAll()
+            cc.audioEngine.stopAll()
         }
     }
 
-    /** 播放某一个声音:play/resume */
-    static async play(sound: keyof typeof C.SOUND) {
-        if (!this.get_sound_switch()) { return }
-        let info = FSound.ins.map_sound_ins.get(sound)
-        // 载入audio clip资源
-        if (!info.clip) {
-            info.clip = await G.load_res(`${C.PATH}/${info.url}`, cc.AudioClip)
+    /** 播放一个声音 */
+    export async function play(key: keyof typeof SOUND) {
+        if (!get_sound_switch()) { return }
+        let info = map_sound.get(key)
+        if (!info) {
+            info = SOUND[key]
+            map_sound.set(key, info)
         }
+        // 载入audio-clip资源
         if (!info.clip) {
-            FLog.error(`@FSound: audio clip no exsit, url=${info.url}`)
-            return
+            info.clip = await G.load_res(`${SOUND_PATH}/${info.url}`, cc.AudioClip)
         }
+        if (!info.clip) { return }
+        // 特殊情况:bgm类型,如果已经在播放,则跳过
         if (info.loop) {
-            // bgm类型,循环播放,只需要播放1次即可
-            switch (cc.audioEngine.getState(info.id)) {
-                case cc.audioEngine.AudioState.ERROR: case cc.audioEngine.AudioState.STOPPED:
-                    info.id = cc.audioEngine.play(info.clip, info.loop, info.volume)
-                    break;
-                case cc.audioEngine.AudioState.PAUSED:
-                    cc.audioEngine.resume(info.id)
-                    break;
-                default: case cc.audioEngine.AudioState.INITIALZING: case cc.audioEngine.AudioState.PLAYING:
-                    break;
+            let state = cc.audioEngine.getState(info.id)
+            if ([cc.audioEngine.AudioState.INITIALZING, cc.audioEngine.AudioState.PLAYING].includes(state)) {
+                return
             }
-        } else {
-            // 普通音效类型,重复播放,相互独立
-            info.id = cc.audioEngine.play(info.clip, info.loop, info.volume)
         }
+        info.id = cc.audioEngine.play(info.clip, info.loop, info.volume)
     }
 
-    /** 停止某一个声音:stop/pause */
-    static stop(sound: keyof typeof C.SOUND) {
-        let info = FSound.ins.map_sound_ins.get(sound)
-        if (info.loop) {
-            // bgm类型,pause
-            cc.audioEngine.pause(info.id)
-        } else {
-            // 普通音效类型,stop
-            cc.audioEngine.stop(info.id)
-        }
+    /** 停止某一个声音 */
+    export function stop(key: keyof typeof SOUND) {
+        let info = map_sound.get(key)
+        cc.audioEngine.stop(info.id)
     }
 
-    // 常用声音
-
-    static play_bgm() {
+    /** 常用:播放bgm */
+    export function play_bgm() {
         FSound.play("bgm")
     }
 
-    static play_btn() {
+    /** 常用:播放按键音 */
+    export function play_btn() {
         FSound.play("btn")
     }
 
