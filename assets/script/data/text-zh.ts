3e6f8cbff42b87d5145f753c86a784a9a8f040f5
fengyong <fkworld@foxmail.com>
2019-08-20 14:38:07 +0800

    修改text数据文件的命名方式

---
+++ assets/script/data/text-zh.ts
@@ -0,0 +1,9 @@
+/** i18n-中文  */
+export const zh = {
+
+    "panel_loading_game_info": "游戏名称:{0}\n开发者:{1}\n版本号:{2}-{3}",
+
+    "panel_message_title": "消息",
+    "panel_message_yes": "是",
+    "panel_message_no": "否",
+}
