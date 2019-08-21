e646207ed68f7ab9d4e6689b0eb0e2746be039dd
fengyong <fkworld@foxmail.com>
2019-08-21 15:29:13 +0800

    脚本管理脚本 namespcae restful

--- assets/script/framework/FVersion.ts
+++ assets/script/framework/FVersion.ts
@@ -1,50 +1,43 @@
-const C = {
-    NAME: "cocos-game-framework",
-    CREATOR: "skyfox-fengyong",
-    VERSION_NUMBER: "0.0",
-    VERSION_TIME: "2019-5-14",
-    VERSION: ["dev"] as TypeVersion[],
-}
-
 /**
- * 版本类型
- * 1. dev   开发版本
- * 2. rc    正式版本
+ * 版本类型;注意使用正向描述(即不要使用no-*这样的描述)
+ * - dev   开发版本
+ * - rc    正式版本
  */
 type TypeVersion = "dev" | "rc";
 
 /**
- * [M] 环境管理,版本管理
+ * [framework] 环境+版本管理
  * - 运行环境,使用引擎自带宏变量,编辑器环境,发布环境等,具体参考https://docs.cocos.com/creator/api/zh/modules/GLOBAL-MACROS.html
  * - 允许多版本标记同时存在
  */
-export class FVersion {
+export namespace FVersion {
+
+    /** 游戏信息 */
+    const game_info = {
+        name: "cocos-game-framework",
+        creator: "skyfox-fengyong",
+        version_number: "0.1",
+        version_time: "2019-8-21",
+    }
+    /** 组合版本 */
+    const versions: Set<TypeVersion> = new Set(["dev"] as TypeVersion[])
 
     // 获取游戏信息
 
-    static get_name() { return C.NAME }
-
-    static get_creator() { return C.CREATOR }
-
-    static get_version_number() { return C.VERSION_NUMBER }
-
-    static get_version_time() { return C.VERSION_TIME }
-
-    static get_version_string() { return C.VERSION.toString() }
+    export function get_name() { return game_info.name }
+    export function get_creator() { return game_info.creator }
+    export function get_version_number() { return game_info.version_number }
+    export function get_version_time() { return game_info.version_time }
+    export function get_version_string() { return `${versions}` }
 
     // 运行环境判定
 
-    /** 编辑器环境 */
-    static is_editor() { return CC_EDITOR }
-
-    /** 预览环境 */
-    static is_preview() { return CC_PREVIEW }
+    export function is_editor() { return CC_EDITOR }        // 编辑器环境
+    export function is_preview() { return CC_PREVIEW }      // 预览环境
 
     // 运行版本判定
 
-    /** 开发版本 */
-    static is_dev() { return C.VERSION.includes("dev") }
-
-    static is_rc() { return C.VERSION.includes("rc") }
+    export function is_dev() { return versions.has("dev") } // 开发版本
+    export function is_rc() { return versions.has("rc") }   // 正式版本
 
 }
