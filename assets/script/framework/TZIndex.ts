0236191c038292de8a2a1d2c6381275311a835c4
fengyong <fkworld@foxmail.com>
2019-08-26 17:59:54 +0800

    修改5个T系列脚本的api和内部实现

--- assets/script/framework/TZIndex.ts
+++ assets/script/framework/TZIndex.ts
@@ -3,22 +3,22 @@ import { FVersion } from "./FVersion";
 const { ccclass, property, menu } = cc._decorator
 
 /**
- * [framework-T] z轴顺序(zIndex)工具
- * - [注意] 仅在编辑器中执行，不在onLoad()时执行
+ * [T] 调整node-zIndex(节点渲染次序)工具
+ * - [注意] 仅在编辑器中执行，不在onLoad时执行
  */
 @ccclass
 @menu("t/TZIndex")
 export class TZIndex extends cc.Component {
 
-    @property({ tooltip: '当前的zIndex', readonly: true })
+    @property({ tooltip: "当前的zIndex", readonly: true })
     private now_z_index = 0
 
-    @property({ tooltip: '更改的zIndex' })
+    @property({ tooltip: "更改的zIndex" })
     private z_index = 0
 
     @property({ tooltip: "编辑器操作" })
-    private get do_editor() { return false }
-    private set do_editor(v: boolean) {
+    private get E() { return false }
+    private set E(v: boolean) {
         FVersion.is_editor() && this.update_z_index()
     }
 
