098af6267b1992ac01597370f174ecccfd362081
fengyong <fkworld@foxmail.com>
2019-11-18 19:02:37 +0800

    删除不必要的panel

--- assets/script/panel/PanelExample.ts
+++ assets/script/panel/PanelExample.ts
@@ -6,19 +6,19 @@ const C = {
 }
 
 /**
- * [Panel] PanelBase
- * - [建议] 直接通过复制PanelBase.prefab/PanelBase.ts两个文件来新建窗口,复制完毕后注意修改相应的名称.
+ * [Panel] PanelExample
+ * - [建议] 直接通过复制 PanelExample.prefab/PanelExample.ts两个文件来新建窗口,复制完毕后注意修改相应的名称.
  * - [注意] 通过on_open/on_close方法配置界面打开/关闭的处理逻辑(一般是动画).其中on_open会在onLoad之后执行.
  */
 @ccclass
-@menu("panel/PanelBase")
-export class PanelBase extends cc.Component implements FPanel.FPanelTemplate {
+@menu("panel/PanelExample")
+export class PanelExample extends cc.Component implements FPanel.FPanelTemplate {
 
     static context = FPanel.set_panel_context({
-        path: "PanelBase",
+        path: "PanelExample",
         type_open: null as {},
         type_close: null as {},
-        ins: null as PanelBase,
+        ins: null as PanelExample,
     })
 
     async on_open(params) {
