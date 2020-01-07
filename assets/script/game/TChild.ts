790a10b6812fdcd83052eb863721f3e3283911d8
fengyong <fkworld@foxmail.com>
2020-01-07 11:38:19 +0800

    Update TChild.ts
    修改 if 语句的格式

--- assets/script/game/TChild.ts
+++ assets/script/game/TChild.ts
@@ -15,7 +15,7 @@ export class TChild extends cc.Component {
      */
     static get_child(parent_node: cc.Node, child_nodename: string): cc.Node {
         let r = parent_node.getComponent(TChild).child_list.find(v => v.name === child_nodename)
-        if(!r) cc.error(`@TChild: node不存在, name=${name}`)
+        !r && cc.error(`@TChild: node不存在, name=${name}`)
         return r
     }
 
