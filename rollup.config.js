990c1ae91f69d512fe58f97c0a246b8fac4e710e
fengyong <fkworld@foxmail.com>
2020-05-08 11:40:05 +0800

    fix: 修改rollup的配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,11 @@
 import typescript from "rollup-plugin-typescript2";
-import { uglify } from "rollup-plugin-uglify";
+import { terser } from "rollup-plugin-terser";
 
 export default {
     input: "framework/index.ts",
     plugins: [
         typescript(),
-        uglify(),
+        terser(),
     ],
     output: {
         file: "framework-dist/framework-v1.0.0.js",
