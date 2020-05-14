33c5f5c8f460cdc70c36cef64a73adad0997ae5d
fengyong <fkworld@foxmail.com>
2020-05-14 13:50:33 +0800

    feat: 新增prettier配置

--- rollup.config.js
+++ rollup.config.js
@@ -10,18 +10,14 @@ export default {
       useTsconfigDeclarationDir: true,
       tsconfigOverride: {
         compilerOptions: {
-          // 覆盖tsconfig.json
+          // 覆盖 tsconfig.json
           target: "ES5",
           module: "ESNext",
           // 自动生成声明文件
           declaration: true,
           declarationDir: "./types/fy/",
         },
-        include: [
-          "framework/src/*",
-          "creator.d.ts",
-          "types/game.d.ts",
-        ]
+        include: ["framework/src/*", "creator.d.ts", "types/game.d.ts"],
       },
     }),
     // 用于压缩js文件，暂时不使用
@@ -30,9 +26,9 @@ export default {
   output: {
     // 输出文件
     file: "./framework/dist/fy-1.0.0.js",
-    // 需要在creator中作为插件脚本使用，因此必须要生成为umd模块
+    // 需要在 creator 中作为插件脚本使用，因此必须要生成为 umd 模块
     format: "umd",
-    // umd包名。注意此处需要更改生成的index.d.ts声明文件，添加一行：export as namespace {name}
+    // umd包名
     name: "fy",
-  }
+  },
 };
