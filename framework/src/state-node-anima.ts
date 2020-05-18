70b0d2d692443cee30e8216943151a530e13fa0c
fengyong <fkworld@foxmail.com>
2020-05-18 11:46:38 +0800

    feat: 新增节点动画的获取当前节点动画状态方法

--- framework/src/state-node-anima.ts
+++ framework/src/state-node-anima.ts
@@ -2,22 +2,40 @@
  * 简单节点动画
  */
 export namespace SimpleNodeAnima {
-  /** 保存在 node 上的 key */
-  const SAVE_KEY = Symbol();
+  /** 动画参数 */
+  interface ParamAnima {
+    time?: number; // 时间，默认为 0.3
+    delay?: number; // 延迟，默认为 0
+    ease?: cc.tweenEasing; // ease 函数，默认为 linear
+  }
+
+  /** 保存在 node 上的状态存储 */
+  const KEY_STATES = Symbol();
+
+  /** 保存在 node 上的当前状态 */
+  const KEY_STATE_NOW = Symbol();
 
   /**
    * 设置节点的状态信息
    * @param node
-   * @param state
+   * @param init_state
+   * @param states
    */
   export const set_all = (
     node: cc.Node,
     init_state: string,
     states: { [key: string]: Partial<cc.Node> }
   ) => {
-    node[SAVE_KEY] = states;
-    init_state && no_anima(node, init_state);
+    node[KEY_STATES] = states;
+    node[KEY_STATE_NOW] = init_state;
+    no_anima(node, init_state);
   };
+
+  /**
+   * 获取当前的节点动画状态
+   * @param node
+   */
+  export const get_now = (node: cc.Node) => node[KEY_STATE_NOW];
 
   /**
    * 无动画，直接至某个节点为某个状态
@@ -25,7 +43,8 @@ export namespace SimpleNodeAnima {
    * @param to
    */
   export const no_anima = (node: cc.Node, to: string) => {
-    cc.tween(node).set(node[SAVE_KEY][to]).start();
+    node[KEY_STATE_NOW] = to;
+    cc.tween(node).set(node[KEY_STATES][to]).start();
   };
 
   /**
@@ -34,22 +53,15 @@ export namespace SimpleNodeAnima {
    * @param to 目标状态的key
    * @param params 动画参数
    */
-  export const anima = async (
-    node: cc.Node,
-    to: string,
-    params: {
-      time?: number; // 时间，默认为0.3
-      delay?: number; // 延迟，默认为0
-      ease?: cc.tweenEasing; // ease函数，默认为linear
-    }
-  ) => {
+  export const anima = async (node: cc.Node, to: string, params: ParamAnima) => {
     params.time = params.time ?? 0.3;
     params.delay = params.delay ?? 0;
     params.ease = params.ease ?? "linear";
+    node[KEY_STATE_NOW] = to;
     await new Promise(res => {
       cc.tween(node)
         .delay(params.delay)
-        .to(params.time, node[SAVE_KEY][to], { easing: params.ease })
+        .to(params.time, node[KEY_STATES][to], { easing: params.ease })
         .call(res)
         .start();
     });
