727da18307229cd5fe7a898168179eba9a77f8c1
fengyong <fkworld@foxmail.com>
2020-06-10 14:35:02 +0800

    chore: 修改jest配置，补充一个测试当前文件的task

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,6 @@ module.exports = {
   preset: "ts-jest",
   // 缺少global-afterEnv版本的配置，所以每个模块执行前都会执行1次，花费时间约为330ms
   setupFilesAfterEnv: ["./__test__/jest.setup.js"],
-  coveragePathIgnorePatterns: ["/node_modules/", "/__test__/cocos2d-js-min.js"],
+  // 测试覆盖率
+  collectCoverageFrom: ["src/**/*"],
 };
