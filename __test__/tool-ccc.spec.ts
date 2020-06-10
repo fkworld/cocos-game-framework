312db51c1a409d22587fcc10fed62be5f7bba313
fengyong <fkworld@foxmail.com>
2020-06-10 19:29:20 +0800

    feat: 新增get_node_childs方法用于获取子节点

--- __test__/tool-ccc.spec.ts
+++ __test__/tool-ccc.spec.ts
@@ -1,16 +1,39 @@
-/**
- * 注意，涉及到游戏生命周期的函数，则不可在node环境中测试。
- */
-
 import * as ccc from "../src/tool-ccc";
 
-test("get_filename", () => {
-  expect(ccc.get_filename("a.png")).toBe("a");
-  expect(ccc.get_filename("path/a.png")).toBe("a");
-  expect(ccc.get_filename("path/a")).toBe("a");
-  expect(ccc.get_filename("db://path/a.png")).toBe("a");
+describe(ccc.get_filename.name, () => {
+  test("", () => {
+    expect(ccc.get_filename("a.png")).toBe("a");
+    expect(ccc.get_filename("path/a.png")).toBe("a");
+    expect(ccc.get_filename("path/a")).toBe("a");
+    expect(ccc.get_filename("db://path/a.png")).toBe("a");
+  });
 });
 
-test("to_editor_url", () => {
-  expect(ccc.to_editor_url("a.png")).toBe("db://assets/resources/a.png");
+describe(ccc.to_editor_url.name, () => {
+  test("", () => {
+    expect(ccc.to_editor_url("a.png")).toBe("db://assets/resources/a.png");
+  });
+});
+
+describe(ccc.get_node_childs.name, () => {
+  let n = new cc.Node();
+  let c1 = new cc.Node("c1");
+  let c2 = new cc.Node("c2");
+  let c2c3 = new cc.Node("c2c3");
+  c2c3.addComponent(cc.Label);
+  n.addChild(c1);
+  n.addChild(c2);
+  c2.addChild(c2c3);
+  test("success", () => {
+    let chidls = ccc.get_node_childs(
+      n,
+      { c1: cc.Node, c2: cc.Node, c2c3: cc.Label },
+      { c2c3: "c2/c2c3" },
+    );
+    expect(chidls.self).toEqual(n);
+    expect(chidls.c1).toEqual(c1);
+    expect(chidls.c2).toEqual(c2);
+    expect(chidls.c2c3.node).toEqual(c2c3);
+    expect(chidls.c2c3).toBeInstanceOf(cc.Label);
+  });
 });
