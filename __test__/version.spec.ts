e4967d399125297adc5abc1fda4d3e330ff23e2d
fengyong <fkworld@foxmail.com>
2020-06-22 17:01:35 +0800

    refactor: 完善version模块

--- __test__/version.spec.ts
+++ __test__/version.spec.ts
@@ -1,6 +1,29 @@
-import * as version from "../src/version";
+import { _init_version, _add_version, has_version, _del_version } from "../src/version";
 
-test("模块初始化", () => {
-  version._init_version({ resetLocal: 1, a: 0, b: 1 }, {});
-  expect(version.version_center.get_keys()).toEqual(["resetLocal", "b"]);
+beforeEach(() => {
+  _init_version({ reset_local: 1, a: 0, b: 1 });
+});
+
+describe(has_version.name, () => {
+  test("", () => {
+    expect(has_version("a")).toBe(false);
+    expect(has_version("b")).toBe(true);
+  });
+});
+
+describe(_add_version.name, () => {
+  test("", () => {
+    _add_version("c");
+    expect(has_version("c")).toBe(true);
+  });
+});
+
+describe(_del_version.name, () => {
+  test("", () => {
+    expect(has_version("c")).toBe(false);
+    _add_version("c");
+    expect(has_version("c")).toBe(true);
+    _del_version("c");
+    expect(has_version("c")).toBe(false);
+  });
 });
