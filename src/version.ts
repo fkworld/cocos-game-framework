e0ac95c1c8e4be380a46eb06619d12e09c7715f8
fengyong <fkworld@foxmail.com>
2020-06-22 17:01:35 +0800

    refactor: 完善version模块

--- src/version.ts
+++ src/version.ts
@@ -1,27 +1,21 @@
 /**
  * 版本管理模块
- * - 通过版本标记管理游戏版本。
- * - 使用正向含义标记。
- * - 需要在运行时初始化，传入2个配置数据：ConfigVersion，ConfigVersionInfo。
+ * - 需要初始化，传入版本标记信息
+ * @see https://www.yuque.com/fengyong/game-develop-road/ylcktc
+ * @see https://www.yuque.com/fengyong/game-develop-road/fq9a6f
  */
-
-import { log, LogLevel } from "./log";
-import { StateTable } from "./tool-state-table";
 
 /**
  * 版本标记信息
- * - key 表示版本标记
- * - value 表示是否包含此标记，1表示包含，0表示不包含
+ * @property key 表示版本标记
+ * @property value 表示是否包含此标记，1表示包含，0表示不包含
  */
 export interface ConfigVersion {
-  /** 是否清空本地存储 */
-  resetLocal: number;
   [k: string]: number;
 }
 
 /**
  * 版本额外信息
- * - 仅作为显示使用，如果没有，可以传undefined
  */
 export interface ConfigVersionInfo {
   /** 项目名称 */
@@ -37,29 +31,68 @@ export interface ConfigVersionInfo {
 }
 
 /** 版本标记 */
-export let version_center: StateTable<string, number>;
+let versions: Set<string>;
 
 /**
  * 初始化版本信息
  * @param config 版本标记信息
- * @param info 版本额外信息
+ * @since 1.0.0
  */
-export function _init_version(
-  config: ConfigVersion = { resetLocal: 1 },
-  info: ConfigVersionInfo = {},
-) {
-  version_center = new StateTable(config);
-  version_center.get_all().forEach((v, k) => {
-    !v && version_center.del(k);
-  });
+export function _init_version(config: ConfigVersion = { reset_local: 1 }): void {
+  versions = new Set(
+    Object.keys(config).filter(key => {
+      return !!config[key];
+    }),
+  );
 }
 
-/** dev模式下全局变量，针对类的装饰器 */
-export function DeDevConsole(constructor: any) {
+/**
+ * 是否包含某个版本标记
+ * @since 1.0.0
+ * @param version_key
+ */
+export function has_version(version_key: string): boolean {
+  return versions.has(version_key);
+}
+
+/**
+ * 新增一个版本标记，一般情况下不要使用
+ * @since 1.0.0
+ * @param version_key
+ */
+export function _add_version(version_key: string): void {
+  versions.add(version_key);
+}
+
+/**
+ * 删除一个版本标记，一般情况下不要使用
+ * @since 1.0.0
+ * @param version_key
+ */
+export function _del_version(version_key: string): void {
+  versions.delete(version_key);
+}
+
+/**
+ * dev模式下全局变量装饰器，针对类
+ * @since 1.0.0
+ * @example
+ * &#64;DE_DEV_CONSOLE
+ * export class A {}
+ */
+export function DE_DEV_CONSOLE(constructor: new () => unknown): void {
   CC_DEV && (window[constructor.name] = constructor);
 }
 
-/** dev模式下全局变量，针对模块 */
-export function DeDevConsoleNamespace(name: string, namespace: any) {
+/**
+ * dev模式下全局变量装饰器，针对模块
+ * - 实际上并非作为装饰器的用法
+ * @since 1.0.0
+ * @example
+ * export namespace A {
+ * DE_DEV_CONSOLE_NAMESPACE("A", A);
+ * }
+ */
+export function DE_DEV_CONSOLE_NAMESPACE(name: string, namespace: unknown): void {
   CC_DEV && (window[name] = namespace);
 }
