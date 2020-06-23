6434ff7cbe66bf5404481033f4a741a1aae77c12
fengyong <fkworld@foxmail.com>
2020-06-23 09:47:18 +0800

    refactor: 完善状态表模块

--- __test__/tool-state-table.spec.ts
+++ __test__/tool-state-table.spec.ts
@@ -1,25 +1,35 @@
 import { StateTable } from "../src/tool-state-table";
 
-const t = new StateTable({ a: 1, b: 2, c: 3, d: 4 });
+let state_table = new StateTable({ a: 1, b: 2, c: 3, d: 4 });
 
-test("has", () => {
-  expect(t.has("a")).toBe(true);
-  expect(t.has("e" as any)).toBe(false);
+describe(StateTable.prototype.has.name, () => {
+  test("", () => {
+    expect(state_table.has("a")).toBe(true);
+    expect(state_table.has("e" as any)).toBe(false);
+  });
 });
 
-test("get", () => {
-  expect(t.get("a")).toBe(1);
-  expect(t.get("e" as any)).toBeUndefined();
+describe(StateTable.prototype.get.name, () => {
+  test("", () => {
+    expect(state_table.get("a")).toBe(1);
+  });
+  test("undefined", () => {
+    expect(state_table.get("e" as any)).toBeUndefined();
+  });
 });
 
-test("add", () => {
-  expect(t.has("f" as any)).toBe(false);
-  t.add("f" as any);
-  expect(t.has("f" as any)).toBe(true);
+describe(StateTable.prototype._add.name, () => {
+  test("", () => {
+    expect(state_table.has("f" as any)).toBe(false);
+    state_table._add("f" as any);
+    expect(state_table.has("f" as any)).toBe(true);
+  });
 });
 
-test("del", () => {
-  expect(t.has("d")).toBe(true);
-  t.del("d" as any);
-  expect(t.has("d")).toBe(false);
+describe(StateTable.prototype._del.name, () => {
+  test("", () => {
+    expect(state_table.has("d")).toBe(true);
+    state_table._del("d" as any);
+    expect(state_table.has("d")).toBe(false);
+  });
 });
