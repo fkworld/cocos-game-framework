5ede9ccaf2e838ebe066194d8dc2d1b340ae7781
fengyong <fkworld@foxmail.com>
2020-06-23 18:33:52 +0800

    refactor: 完善文字数据模块

--- src/text.ts
+++ src/text.ts
@@ -1,94 +1,123 @@
 /**
- * 多语言模块
- * - 需要在编辑器中，运行时初始化，传入2个配置数据：ConfigLanguage，编辑器语言
+ * 文字数据模块
+ * - 需要初始化，传入文字配置数据，编辑器默认语言，运行时默认语言
+ * @see https://www.yuque.com/fengyong/game-develop-road/gepz3f
  */
 
 import { event_center } from "./event";
 import { get_local, set_local } from "./local";
 import { log, LogLevel } from "./log";
 import { get_template_string } from "./tool";
-import { load_res } from "./tool-ccc";
+import { load_res_async } from "./tool-ccc";
 
 /** 事件：语言更改 */
 export const EVENT_LANGUAGE_CHANGE = "text/language-change";
+/** 语言的本地存储key */
+const KEY_LANGUAGE = "text/language";
 
 /**
  * 语言配置
- * - key 表示语言
- * - value 表示语言配置文件
+ * @property key 表示语言
+ * @property value 表示语言配置文件
  */
 export interface ConfigLanguage {
   /** 默认为中文 */
-  chinese: { [k: string]: string };
+  chinese?: { [k: string]: string };
   [k: string]: ConfigLanguage["chinese"];
 }
 
-/** 配置 */
-let languages: ConfigLanguage;
-
+/** 语言配置：语言key，字符key，字符value */
+let languages: Map<string, Map<string, string>>;
 /** 编辑器默认语言 */
-let editor_language: string;
+let language_editor: string;
+/** 运行时默认语言 */
+let language_runtime: string;
 
 /**
- * 在编辑器中初始化 text 模块
+ * 初始化文字数据模块
+ * @since 1.0.0
  * @param config
  * @param editor 编辑器默认语言
+ * @param runtime 运行时默认语言
  */
-export function _init_text(config: ConfigLanguage = { chinese: {} }, editor: string = "chinese") {
-  languages = config;
-  editor_language = editor;
-  !languages[editor_language] && log(LogLevel.IMPORTANT_ERROR, "无法载入编辑器text语言");
+export function _init_text(
+  config: ConfigLanguage = {},
+  editor = "chinese",
+  runtime = "chinese",
+): void {
+  languages = new Map(
+    Object.entries(config).map(([key_language, config_language]) => {
+      return [key_language, new Map(Object.entries(config_language))];
+    }),
+  );
+  language_editor = editor;
+  language_runtime = runtime;
+  !languages.has(language_editor) && log(LogLevel.ImportantError, "无法载入编辑器语言");
+  !languages.has(language_runtime) && log(LogLevel.ImportantError, "无法载入运行时语言");
 }
 
-/** 获取当前的语言 key */
-export function get_language() {
-  return get_local("language");
+/**
+ * 获取当前的语言key
+ * - 区分编辑器中和运行时
+ * @since 1.0.0
+ */
+export function get_language(): string {
+  return CC_EDITOR ? language_editor : get_local(KEY_LANGUAGE, language_runtime);
 }
 
 /**
  * 修改当前语言
- * @param new_language
+ * @since 1.0.0
+ * @param language_key
  */
-export function change_language(new_language: string) {
-  set_local("language", new_language);
+export function change_language(language_key: string): void {
+  set_local(KEY_LANGUAGE, language_key);
   event_center.emit(EVENT_LANGUAGE_CHANGE);
 }
 
 /**
- * 获取语言数据，如果获取失败，则返回 key
- * @param key
+ * 获取语言数据，如果获取失败，则返回key
+ * @since 1.0.0
+ * @param text_key
  * @param params
  */
-export function get_text(key: string, ...params: string[]) {
-  let language = CC_EDITOR ? editor_language : get_language();
-  let text = languages[language][key];
+export function get_text(text_key: string, ...params: string[]): string {
+  let text = languages?.get(get_language())?.get(text_key);
   if (text) {
     return get_template_string(text, ...params);
   } else {
-    log(LogLevel.ERROR, `key不存在, key=${key}`);
-    return key;
+    log(LogLevel.Error, `key不存在, key=${text_key}`);
+    return text_key;
   }
 }
 
 /**
  * 设置节点的text数据
+ * - 支持3种组件，cc.Label/cc.RichText/cc.Sprite
+ * @since 1.0.0
  * @param node
  * @param key
  * @param params
  */
-export function set_node_text(node: cc.Node, key: string, ...params: string[]) {
+export function set_node_text(node: cc.Node, key: string, ...params: string[]): void {
   let result = get_text(key, ...params);
-  if (node.getComponent(cc.Label)) {
-    node.getComponent(cc.Label).string = result;
+  // label
+  let label = node.getComponent(cc.Label);
+  if (label) {
+    label.string = result;
     return;
   }
-  if (node.getComponent(cc.RichText)) {
-    node.getComponent(cc.RichText).string = result;
+  // richtext
+  let richtext = node.getComponent(cc.RichText);
+  if (richtext) {
+    richtext.string = result;
     return;
   }
-  if (node.getComponent(cc.Sprite)) {
-    load_res(result, cc.SpriteFrame).then(v => {
-      node.getComponent(cc.Sprite).spriteFrame = v;
+  // sprite
+  let sprite = node.getComponent(cc.Sprite);
+  if (sprite) {
+    load_res_async(result, cc.SpriteFrame).then(v => {
+      sprite.spriteFrame = v;
     });
     return;
   }
