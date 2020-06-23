dd503128b5a5f4f7f65c0475023b2bbe181294b5
fengyong <fkworld@foxmail.com>
2020-06-23 12:03:34 +0800

    refactor: 完善节点动画模块

--- src/tool-node-anima.ts
+++ src/tool-node-anima.ts
@@ -1,71 +1,99 @@
+/** 其他信息 */
+interface Other {
+  /** 当前状态 */
+  now: string;
+  /** 动画时间 */
+  time?: number;
+  /** 动画延迟 */
+  delay?: number;
+  /** 动画ease函数 */
+  ease?: cc.TweenEasing;
+}
+
+/** 保存在node上的状态存储 */
+const KEY_STATES = Symbol();
+/** 保存在node上的其他信息存储 */
+const KEY_OTHER = Symbol();
+
+/** 获取node上的状态信息 */
+function get_states_by_node(node: cc.Node): Partial<cc.Node> {
+  return node[KEY_STATES];
+}
+/** 获取node上的其他信息 */
+function get_others_by_node(node: cc.Node): Other {
+  return node[KEY_OTHER];
+}
+
 /**
- * 简单节点动画
+ * 设置节点的状态信息
+ * @since 1.0.0
+ * @param node
+ * @param states
+ * @param others
+ * @example
+ * let n = new cc.Node();
+ * let states = { show: { width: 100 }, hide: { width: 0 } };
+ * let others = { now: "hide" };
+ * set_node_anima(n, states, others);
  */
-export namespace SimpleNodeAnima {
-  /** 动画参数 */
-  interface ParamAnima {
-    time?: number; // 时间，默认为0.3
-    delay?: number; // 延迟，默认为0
-    ease?: cc.TweenEasing; // ease函数，默认为linear
-  }
+export function set_node_anima(
+  node: cc.Node,
+  states: { [key: string]: Partial<cc.Node> },
+  others: Other,
+): void {
+  node[KEY_STATES] = states;
+  node[KEY_OTHER] = others;
+  node_anima_not(node, others.now);
+}
 
-  /** 保存在node上的状态存储 */
-  const KEY_STATES = Symbol();
+/**
+ * 获取当前的节点动画状态
+ * @since 1.0.0
+ * @param node
+ */
+export function get_node_anima_now(node: cc.Node): string {
+  return get_others_by_node(node).now;
+}
 
-  /** 保存在node上的当前状态 */
-  const KEY_STATE_NOW = Symbol();
+/**
+ * 无动画，直接修改某个节点为某个状态
+ * @since 1.0.0
+ * @param node
+ * @param target
+ * @example
+ * let n = new cc.Node();
+ * set_node_anima(n, { show: { width: 100 }, hide: { width: 0 } }, { now: "hide" });
+ * node_anima_not(n, "show");
+ */
+export function node_anima_not(node: cc.Node, target: string): void {
+  get_others_by_node(node).now = target;
+  cc.tween(node).set(get_states_by_node(node)[target]).start();
+}
 
-  /**
-   * 设置节点的状态信息
-   * @param node
-   * @param init_state
-   * @param states
-   */
-  export function set_all(
-    node: cc.Node,
-    init_state: string,
-    states: { [key: string]: Partial<cc.Node> },
-  ) {
-    node[KEY_STATES] = states;
-    node[KEY_STATE_NOW] = init_state;
-    no_anima(node, init_state);
-  }
-
-  /**
-   * 获取当前的节点动画状态
-   * @param node
-   */
-  export function get_now(node: cc.Node) {
-    return node[KEY_STATE_NOW];
-  }
-
-  /**
-   * 无动画，直接至某个节点为某个状态
-   * @param node
-   * @param to
-   */
-  export function no_anima(node: cc.Node, to: string) {
-    node[KEY_STATE_NOW] = to;
-    cc.tween(node).set(node[KEY_STATES][to]).start();
-  }
-
-  /**
-   * 动画：从目前状态通过动画迁移到目标状态
-   * @param node
-   * @param to 目标状态的key
-   * @param params 动画参数
-   */
-  export async function anima(node: cc.Node, to: string, params: ParamAnima) {
-    params.time = params.time ?? 0.3;
-    params.delay = params.delay ?? 0;
-    params.ease = params.ease ?? "linear";
-    node[KEY_STATE_NOW] = to;
-    await new Promise(res => {
-      cc.tween(node)
-        .delay(params.delay)
-        .to(params.time, node[KEY_STATES][to], { easing: params.ease })
-        .call(res)
-        .start();
-    });
-  }
+/**
+ * 有动画，通过简单的动画效果修改某个节点为某个状态
+ * @since 1.0.0
+ * @param node
+ * @param target
+ * @param params 动画参数
+ * @example
+ * let n = new cc.Node();
+ * set_node_anima(n, { show: { width: 100 }, hide: { width: 0 } }, { now: "hide" });
+ * node_anima(n, "show");
+ */
+export async function node_anima(
+  node: cc.Node,
+  target: string,
+  params: Omit<Other, "now"> = {},
+): Promise<void> {
+  get_others_by_node(node).now = target;
+  await new Promise(res => {
+    cc.tween(node)
+      .delay(params.delay ?? get_others_by_node(node).delay ?? 0)
+      .to(params.time ?? get_others_by_node(node).time ?? 0.3, get_states_by_node(node)[target], {
+        easing: params.ease ?? get_others_by_node(node).ease ?? "linear",
+      })
+      .call(res)
+      .start();
+  });
 }
