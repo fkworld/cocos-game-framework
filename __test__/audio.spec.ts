af94a7647249cca8b53cee605c0370a476054257
fengyong <fkworld@foxmail.com>
2020-06-23 18:00:14 +0800

    refactor: 完善声音模块

--- __test__/audio.spec.ts
+++ __test__/audio.spec.ts
@@ -1,28 +1,71 @@
-import * as audio from "../src/audio";
+import { _init_local } from "../src";
+import {
+  get_audio,
+  get_music_switch,
+  get_sound_switch,
+  play_audio,
+  pre_audio,
+  reverse_music_switch,
+  reverse_sound_switch,
+  stop_audio,
+  _init_audio,
+} from "../src/audio";
 
-audio._init_audio({
-  test_music: "###muisc",
-  test_sound: "sound",
+beforeEach(() => {
+  _init_local({}, true);
+  _init_audio({ test_music: "###muisc", test_sound: "sound" }, true, true);
 });
 
-test("get_music_switch", () => {
-  expect(audio.get_music_switch()).toBe(true);
+describe(get_music_switch.name, () => {
+  test("", () => {
+    expect(get_music_switch()).toBe(true);
+  });
+  test("初始化时传入false", () => {
+    _init_audio({}, false, false);
+    expect(get_music_switch()).toBe(false);
+  });
 });
 
-test("reverse_music_switch", () => {
-  audio.reverse_music_switch();
-  expect(audio.get_music_switch()).toBe(false);
-  audio.reverse_music_switch();
-  expect(audio.get_music_switch()).toBe(true);
+describe(reverse_music_switch.name, () => {
+  test("", () => {
+    reverse_music_switch();
+    expect(get_music_switch()).toBe(false);
+    reverse_music_switch();
+    expect(get_music_switch()).toBe(true);
+  });
 });
 
-test("get_sound_switch", () => {
-  expect(audio.get_sound_switch()).toBe(true);
+describe(get_sound_switch.name, () => {
+  test("", () => {
+    expect(get_sound_switch()).toBe(true);
+  });
+  test("初始化时传入false", () => {
+    _init_audio({}, false, false);
+    expect(get_sound_switch()).toBe(false);
+  });
 });
 
-test("reverse_sound_switch", () => {
-  audio.reverse_sound_switch();
-  expect(audio.get_sound_switch()).toBe(false);
-  audio.reverse_sound_switch();
-  expect(audio.get_sound_switch()).toBe(true);
+describe(reverse_sound_switch.name, () => {
+  test("", () => {
+    reverse_sound_switch();
+    expect(get_sound_switch()).toBe(false);
+    reverse_sound_switch();
+    expect(get_sound_switch()).toBe(true);
+  });
+});
+
+describe(pre_audio.name, () => {
+  // TODO
+});
+
+describe(get_audio.name, () => {
+  // TODO
+});
+
+describe(play_audio.name, () => {
+  // TODO
+});
+
+describe(stop_audio.name, () => {
+  // TODO
 });
--- src/audio.ts
+++ src/audio.ts
@@ -1,127 +1,145 @@
 /**
  * 声音模块
- * - 用于处理游戏内的声音逻辑
+ * @see https://www.yuque.com/fengyong/game-develop-road/anva67
  */
 
 import { event_center } from "./event";
 import { get_local, set_local } from "./local";
-import { load_res } from "./tool-ccc";
-import { SimpleFSM } from "./tool-fsm";
+import { load_res_async } from "./tool-ccc";
 
 /** 事件：打开音乐开关 */
 export const EVENT_MUSIC_SWITCH_OPEN = "audio/music-switch-open";
+/** 本地存储：音乐开关key */
+const MUSIC_SWITCH_KEY = "audio/music-switch";
+/** 本地存储：音效开关key */
+const SOUND_SWITCH_KEY = "audio/sound-switch";
 
 /**
  * 声音配置
- * - key 中使用###来表示其为音乐类，否则为音效类
- * - value 表示声音文件的路径
+ * @property key 使用###前缀来表示其为音乐，否则为音效
+ * @property value 路径
  */
 export interface ConfigAudio {
   [k: string]: string;
 }
 
-/**
- * 声音状态
- * - prepare 准备
- * - ok 载入成功
- * - error 载入失败
- */
-type AudioState = "prepare" | "ok" | "error";
+/** 声音状态 */
+enum AudioState {
+  /** 准备中 */
+  Prepare,
+  /** 完毕 */
+  Ok,
+  /** 失败 */
+  Error,
+}
 
-/**
- * 声音类型
- * - music 音乐
- * - sound 音效
- */
-type AudioType = "music" | "sound";
+/** 声音类型 */
+enum AudioType {
+  /** 音乐 */
+  Music,
+  /** 音效 */
+  Sound,
+}
 
 /** 声音的实例信息 */
 interface AudioIns {
-  /** 状态机 */
-  fsm: SimpleFSM<AudioState>;
+  /** 状态 */
+  state: AudioState;
   /** 声音类型 */
   type: AudioType;
-  /** 声音文件的 url */
+  /** 声音的url */
   url?: string;
   /** 声音的cc.AudioClip资源，默认为undefined */
   clip?: cc.AudioClip;
-  /** 声音的播放 id */
+  /** 声音的播放id */
   id?: number;
 }
 
 /** 所有声音的实例信息 */
 let audios: Map<string, AudioIns>;
-
-/** 音乐开关 */
-let music_switch: boolean;
-
-/** 音效开关 */
-let sound_switch: boolean;
+/** 音乐开关默认值 */
+let music_switch_default: boolean;
+/** 音效开关默认值 */
+let sound_switch_default: boolean;
 
 /**
- * 初始化
- * - TODO：在初始化中设置声音实例为1，可能会有bug，需要进一步测试
+ * 初始化声音模块
+ * @since 1.0.0
  * @param config
+ * @param music_switch
+ * @param sound_switch
+ * @todo 在初始化中设置声音实例为1，可能会有bug，需要进一步测试
  */
-export function _init_audio(config: ConfigAudio = {}) {
+export function _init_audio(
+  config: ConfigAudio = {},
+  music_switch: boolean,
+  sound_switch: boolean,
+): void {
   audios = new Map(
     Object.entries(config).map(([k, v]) => {
       let ins: AudioIns = {
-        fsm: new SimpleFSM<AudioState>("prepare", {
-          prepare: ["ok", "error"],
-          ok: [],
-          error: [],
-        }),
-        type: k.includes("###") ? "music" : "sound",
+        state: AudioState.Prepare,
+        type: k.startsWith("###") ? AudioType.Music : AudioType.Sound,
         url: v,
       };
-      // TODO：设置单实例，可能会有bug，需要测试
       cc.audioEngine.setMaxAudioInstance(1);
       return [k, ins];
     }),
   );
-  music_switch = get_local("music") === "true";
-  sound_switch = get_local("sound") === "true";
+  music_switch_default = music_switch;
+  sound_switch_default = sound_switch;
 }
 
-/** 获取音乐开关 */
-export function get_music_switch() {
-  return music_switch;
+/**
+ * 获取音乐开关
+ * @since 1.0.0
+ */
+export function get_music_switch(): boolean {
+  return get_local(MUSIC_SWITCH_KEY, `${music_switch_default}`) === "true";
 }
 
-/** 反向音乐开关 */
-export function reverse_music_switch() {
-  music_switch = !music_switch;
-  music_switch ? event_center.emit(EVENT_MUSIC_SWITCH_OPEN) : cc.audioEngine.stopAll();
-  set_local("music", `${music_switch}`);
+/**
+ * 反向音乐开关
+ * @since 1.0.0
+ */
+export function reverse_music_switch(): void {
+  set_local(MUSIC_SWITCH_KEY, `${!get_music_switch()}`);
+  get_music_switch() ? event_center.emit(EVENT_MUSIC_SWITCH_OPEN) : cc.audioEngine.stopAll();
 }
 
-/** 获取音效开关 */
-export function get_sound_switch() {
-  return sound_switch;
+/**
+ * 获取音效开关
+ * @since 1.0.0
+ */
+export function get_sound_switch(): boolean {
+  return get_local(SOUND_SWITCH_KEY, `${sound_switch_default}`) === "true";
 }
 
-/** 反向音乐开关 */
-export function reverse_sound_switch() {
-  sound_switch = !sound_switch;
-  set_local("sound", `${sound_switch}`);
+/**
+ * 反向音效开关
+ * @since 1.0.0
+ */
+export function reverse_sound_switch(): void {
+  set_local(SOUND_SWITCH_KEY, `${!get_sound_switch()}`);
 }
 
 /**
  * 预载入一个audio
+ * @since 1.0.0
  * @param key
  */
-export async function pre_audio(key: string) {
+export async function pre_audio(key: string): Promise<void> {
   let data = audios.get(key);
-  if (!data.clip) {
-    data.clip = await load_res(data.url, cc.AudioClip);
-    data.fsm.try_go_state(data.clip ? "ok" : "error");
+  if (data.state === AudioState.Prepare && !data.clip) {
+    data.clip = await load_res_async(data.url, cc.AudioClip);
+    data.state = data.clip ? AudioState.Ok : AudioState.Error;
   }
   audios.set(key, data);
 }
 
 /**
  * 获取声音实例
+ * @since 1.0.0
  * @param key
  */
 export async function get_audio(key: string): Promise<AudioIns> {
@@ -129,26 +147,34 @@ export async function get_audio(key: string): Promise<AudioIns> {
   return audios.get(key);
 }
 
-/** 播放一个声音 */
-export async function play_audio(key: string) {
+/**
+ * 播放一个声音
+ * @since 1.0.0
+ * @param key
+ */
+export async function play_audio(key: string): Promise<void> {
   let data = await get_audio(key);
   if (
-    data.fsm.is_state("error") ||
-    (data.type === "music" && !music_switch) ||
-    (data.type === "sound" && !sound_switch)
+    data.state === AudioState.Error ||
+    (data.type === AudioType.Music && !get_music_switch()) ||
+    (data.type === AudioType.Sound && !get_sound_switch())
   ) {
     return;
   }
   data.id =
-    data.type === "music"
+    data.type === AudioType.Music
       ? cc.audioEngine.playMusic(data.clip, true)
       : cc.audioEngine.playEffect(data.clip, false);
 }
 
-/** 停止某一个声音 */
-export async function stop_audio(key: string) {
+/**
+ * 停止某一个声音
+ * @since 1.0.0
+ * @param key
+ */
+export async function stop_audio(key: string): Promise<void> {
   let data = await get_audio(key);
-  if (data.fsm.is_state("error")) {
+  if (data.state === AudioState.Error) {
     return;
   }
   data.id && cc.audioEngine.stop(data.id);
