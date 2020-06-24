a0b166d156f8a231f5ff10d08a621505cc9fcf93
fengyong <fkworld@foxmail.com>
2020-06-24 15:54:10 +0800

    refactor: 完善界面模块

--- __test__/panel.spec.ts
+++ __test__/panel.spec.ts
@@ -1,15 +1,23 @@
-import * as panel from "../src/panel";
+import {
+  close_panel,
+  DE_SET_PANEL_CONTEXT,
+  open_panel,
+  PanelBase,
+  _init_panel,
+} from "../src/panel";
 
 jest.spyOn(cc, "instantiate").mockImplementation(() => {
-  let mockedNode = new cc.Node();
-  mockedNode.addComponent(PanelTest);
-  return mockedNode;
+  let node = new cc.Node();
+  node.addComponent(PanelTest);
+  return node;
 });
 
-panel._init_panel(new cc.Node());
+beforeAll(() => {
+  _init_panel(new cc.Node());
+});
 
-@panel.DeSetPanelContext("")
-class PanelTest extends panel.PanelBase {
+@DE_SET_PANEL_CONTEXT("")
+class PanelTest extends PanelBase {
   static ins: PanelTest;
   static a = 0;
   async on_create() {
@@ -24,16 +32,20 @@ class PanelTest extends panel.PanelBase {
   }
 }
 
-test("open_panel", async () => {
-  expect(PanelTest.ins).toBeUndefined();
-  expect(PanelTest.a).toBe(0);
-  await panel.open_panel(PanelTest);
-  expect(PanelTest.ins.node.active).toBe(true);
-  expect(PanelTest.a).toBe(2);
+describe(open_panel.name, () => {
+  test("", async () => {
+    expect(PanelTest.ins).toBeUndefined();
+    expect(PanelTest.a).toBe(0);
+    await open_panel(PanelTest);
+    expect(PanelTest.ins.node.active).toBe(true);
+    expect(PanelTest.a).toBe(2);
+  });
 });
 
-test("close_panel", async () => {
-  await panel.close_panel(PanelTest);
-  expect(PanelTest.ins.node.active).toBe(false);
-  expect(PanelTest.a).toBe(3);
+describe(close_panel.name, () => {
+  test("", async () => {
+    await close_panel(PanelTest);
+    expect(PanelTest.ins.node.active).toBe(false);
+    expect(PanelTest.a).toBe(3);
+  });
 });
