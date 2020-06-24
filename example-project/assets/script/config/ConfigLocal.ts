e38deb7fcf6c0d7827715b1bb85ee8e4bcf44afd
fengyong <fkworld@foxmail.com>
2020-06-24 22:01:16 +0800

    chore: 删除示例项目，将creator.d.ts手动放入types下

--- example-project/assets/script/config/ConfigLocal.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 其值表示默认值和类型，类型只能为number/string/boolean，否则在处理时会异常
- */
-export const CONFIG_LOCAL: fy.ConfigLocal = {
-  language: "chinese",
-  music: true,
-  sound: true,
-};
--- example-project/types/fy.d.ts
+++
@@ -1,4 +0,0 @@
-// 从index中获取导出
-export * from "./src/index";
-// 设置为umd包，包名为fy
-export as namespace fy;
