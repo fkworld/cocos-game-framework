cdb2aa660455b3f9e910a7ac216a98f06c1a2a19
fengyong <fkworld@foxmail.com>
2020-06-24 11:18:31 +0800

    refactor: 完善数据表模块

--- __test__/meta.spec.ts
+++ __test__/meta.spec.ts
@@ -1,7 +1,17 @@
-import * as meta from "../src/meta";
+import {
+  DE_SET_META_CONTEXT,
+  get_meta,
+  get_metas,
+  get_metas_id,
+  MetaBase,
+  _init_meta,
+  _parse_csv,
+  parse_csv_all,
+} from "../src/meta";
 
-beforeAll(() => {
-  meta._init_meta({
+beforeEach(() => {
+  _init_meta({
+    // eslint-disable-next-line @typescript-eslint/naming-convention
     MetaTest: {
       1: { id: "1", name: "fy" },
       2: { id: "2", name: "fyfy" },
@@ -9,11 +19,11 @@ beforeAll(() => {
   });
 });
 
-@meta.DeSetMetaContext("MetaTest")
-class MetaTest extends meta.MetaBase {
-  use_special(s: object): void {
-    this.id = s["id"];
-    this.name = s["name"];
+@DE_SET_META_CONTEXT("MetaTest")
+class MetaTest extends MetaBase {
+  use_special(_s: unknown): void {
+    this.id = _s["id"];
+    this.name = _s["name"];
   }
   use_default(id: string): void {
     this.id = id;
@@ -23,30 +33,38 @@ class MetaTest extends meta.MetaBase {
   name: string;
 }
 
-describe(meta.get_meta.name, () => {
+describe(get_meta.name, () => {
   test("special", () => {
-    expect(meta.get_meta(MetaTest, "1").name).toBe("fy");
-    expect(meta.get_meta(MetaTest, "2").name).toBe("fyfy");
+    expect(get_meta(MetaTest, "1").name).toBe("fy");
+    expect(get_meta(MetaTest, "2").name).toBe("fyfy");
   });
   test("default", () => {
-    expect(meta.get_meta(MetaTest, "3").name).toBe("fy3");
+    expect(get_meta(MetaTest, "3").name).toBe("fy3");
   });
   test("error", () => {
-    @meta.DeSetMetaContext("MetaError")
-    class ErrorMeta extends meta.MetaBase {}
-    expect(ErrorMeta.get_meta_source()).toBeUndefined();
+    @DE_SET_META_CONTEXT("MetaError")
+    class ErrorMeta extends MetaBase {}
+    expect(ErrorMeta._get_meta_source_all()).toBeUndefined();
   });
 });
 
-describe(meta.get_metas.name, () => {
+describe(get_metas.name, () => {
   test("", () => {
-    expect(meta.get_metas(MetaTest)).toHaveLength(2);
-    expect(meta.get_metas(MetaTest)[0]).toBeInstanceOf(MetaTest);
+    expect(get_metas(MetaTest)).toHaveLength(2);
+    expect(get_metas(MetaTest)[0]).toBeInstanceOf(MetaTest);
   });
 });
 
-describe(meta.get_metas_ids.name, () => {
+describe(get_metas_id.name, () => {
   test("", () => {
-    expect(meta.get_metas_ids(MetaTest)).toEqual(["1", "2"]);
+    expect(get_metas_id(MetaTest)).toEqual(["1", "2"]);
   });
+});
+
+describe(_parse_csv.name, () => {
+  test.todo("");
+});
+
+describe(parse_csv_all.name, () => {
+  test.todo("");
 });
--- src/meta.ts
+++ src/meta.ts
@@ -1,8 +1,18 @@
-import { log, LogLevel } from "./log";
-import { to_editor_url, load } from "./tool-ccc";
+/**
+ * 数据表模块
+ * - 需要初始化，传入配置数据
+ * @see https://www.yuque.com/fengyong/game-develop-road/wo07pz
+ * @todo 修改从csv到ts文件的处理方法
+ */
 
+import { log, LogLevel } from "./log";
+import { to_editor_url, load_async } from "./tool-ccc";
+
+/**
+ * meta配置信息，一般是通过csv文件自动生成
+ */
 export type ConfigMeta = {
-  [meta_name: string]: {
+  [metaName: string]: {
     [id: string]: {
       [key: string]: string;
     };
@@ -11,87 +21,135 @@ export type ConfigMeta = {
 
 let metas: ConfigMeta;
 
-export function _init_meta(config: ConfigMeta = {}) {
+/**
+ * 初始化
+ * @since 1.0.0
+ * @param config
+ */
+export function _init_meta(config: ConfigMeta = {}): void {
   metas = config;
 }
 
-/** meta基类，需要被继承 */
+/**
+ * meta基类
+ * @since 1.0.0
+ * @example
+  &#64;fy.DeSetMetaContext("MetaExample")
+  export class MetaExample extends fy.MetaBase {
+    use_special(_s: unknown): void {
+      this.id = _s["id"];
+      this.age = Number.parseInt(_s["age"]);
+    }
+    id: string;
+    age: number;
+  }
+ */
 export class MetaBase {
   /** meta数据表名 */
   static meta_name: string;
-  static get_meta_source(id: string = undefined) {
-    let r = id ? metas?.[this.meta_name]?.[id] : metas?.[this.meta_name];
-    !r && log(LogLevel.WARN, `获取meta源数据失败，meta_name=${this.meta_name}，id=${id}`);
-    return r;
+
+  /**
+   * 获取完整表数据
+   * @since 1.0.0
+   */
+  static _get_meta_source_all(): ConfigMeta[""] {
+    return metas?.[this.meta_name];
   }
+
+  /**
+   * 获取表内单个id的数据
+   * @since
+   * @param id
+   */
+  static _get_meta_source_by_id(id: string): ConfigMeta[""][""] {
+    return metas?.[this.meta_name]?.[id];
+  }
+
   /** 是否是不存在id而使用的默认值 */
   is_default: boolean;
-  /** 创建meta类实例时，对传入的单行源数据进行处理 */
-  use_special(s: object) {}
-  /** 创建meta类实例时，如果没有源数据，则设置为给定的默认值 */
-  use_default(id: string) {
+
+  /**
+   * 创建meta类实例时，对传入的单行源数据进行处理
+   * @since 1.0.0
+   */
+  use_special(_s: unknown): void {}
+
+  /**
+   * 创建meta类实例时，如果没有源数据，则设置为给定的默认值
+   * @since 1.0.0
+   */
+  use_default(id: string): void {
     throw new Error(`meta-id不存在，id=${id}`);
   }
 }
 
 /**
- * 设置meta类的上下文信息
- * @param meta_name meta名
+ * 装饰器函数：设置meta类的上下文信息
+ * @since 1.0.0
+ * @param meta_name
  */
-export function DeSetMetaContext(meta_name: string) {
-  return (constructor: typeof MetaBase) => {
+export function DE_SET_META_CONTEXT(meta_name: string) {
+  return (constructor: typeof MetaBase): void => {
     constructor.meta_name = meta_name;
   };
 }
 
 /**
  * 获取单个的meta
+ * @since 1.0.0
  * @param meta_class
  * @param id
  */
 export function get_meta<T extends typeof MetaBase>(meta_class: T, id: string): InstanceType<T> {
   let meta = new meta_class();
-  let source = meta_class.get_meta_source(id);
+  let source = meta_class._get_meta_source_by_id(id);
   source ? meta.use_special(source) : meta.use_default(id);
   return meta as any;
 }
 
 /**
  * 获取meta数组
+ * @since 1.0.0
  * @param meta_class
  */
 export function get_metas<T extends typeof MetaBase>(meta_class: T): InstanceType<T>[] {
-  return Object.keys(meta_class.get_meta_source()).map(id => get_meta(meta_class, id));
+  return get_metas_id(meta_class).map(id => get_meta(meta_class, id));
 }
 
 /**
  * 获取所有meta的id数组
+ * @since 1.0.0
  * @param meta_class
  */
-export function get_metas_ids<T extends typeof MetaBase>(meta_class: T): string[] {
-  return Object.keys(meta_class.get_meta_source());
+export function get_metas_id<T extends typeof MetaBase>(meta_class: T): string[] {
+  return Object.keys(meta_class._get_meta_source_all());
 }
 
-// TODO 在2.4.0中，由于资源载入方式发生变化，这里也需要进行修改
-
+/** 自动生成的文件名 */
 const AUTO_GENERATE_FILENAME = "csv/auto-generate.ts";
 
 const REGS = {
-  // 注释行标记
+  /** 注释行标记 */
   COMMENT: /^#/,
-  // 属性行标记
+  /** 属性行标记 */
   HEADER: /^@/,
-  // 换行符
+  /** 换行符 */
   NEW_LINE: /\r\n/g,
-  // 单行中的块拆分正则
+  /**
+   * 单行中的块拆分正则
+   * @see https://github.com/fkworld/cocos-game-framework/issues/50
+   * @todo 由于部分浏览器不支持环视，这里会导致在导出为web时出现解析错误
+   */
   LINE: /(?<=,|^)(("[^"]*")+|[^,]*)(?=,|$)/g,
 };
 
 /**
  * 解析csv文件为json对象
+ * @since 1.0.0
  * @param source csv文件内容
+ * @description 需要修改csv到ts文件的方法
  */
-export function _parse_csv(source: string) {
+export function _parse_csv(source: string): Record<string, unknown> {
   // 属性行
   let headers: string[] = [];
   // 拆分行
@@ -110,8 +168,8 @@ export function _parse_csv(source: string) {
       let pieces = line.match(REGS.LINE);
       result[pieces[0]] = Object.fromEntries(
         headers.map((header, index) => {
-          let fix_piece = pieces[index]?.trim().replace(/^"|"$/g, "").replace(/""/g, '"') ?? "";
-          return [header, fix_piece];
+          let piece_fix = pieces[index]?.trim().replace(/^"|"$/g, "").replace(/""/g, '"') ?? "";
+          return [header, piece_fix];
         }),
       );
     }
@@ -120,16 +178,26 @@ export function _parse_csv(source: string) {
 }
 
 /**
- * 将resoueces/csv/路径下的所有csv文件，转换为同路径下的json文件
+ * 将resoueces/csv/路径下的所有csv文件，转换为同路径下的ts文件
+ * @since 1.0.0
+ * @description 需要修改csv到ts文件的方法
+ * @example
+  &#64;property({ tooltip: "点击创建json文件" })
+  private get E() {
+    return false;
+  }
+  private set E(v: boolean) {
+    CC_EDITOR && fy.parse_csv_all();
+  }
  */
-export async function parse_csv_all() {
+export async function parse_csv_all(): Promise<void> {
   let url_target = to_editor_url(AUTO_GENERATE_FILENAME);
   let url_source = to_editor_url(cc.path.dirname(AUTO_GENERATE_FILENAME) + "/*.csv");
   let files: Partial<Editor.assetdb.TypeAssetInfo>[] = await new Promise(res => {
     Editor.assetdb.queryAssets(url_source, "text", (err, results) => res(results));
   });
   let file_texts: cc.TextAsset[] = await Promise.all(
-    files.map(file => load({ type: "uuid", uuid: file.uuid })),
+    files.map(file => load_async({ type: "uuid", uuid: file.uuid })),
   );
   let json = file_texts.reduce((r, text) => {
     r[text.name] = _parse_csv(text.text);
@@ -138,10 +206,10 @@ export async function parse_csv_all() {
   Editor.assetdb.createOrSave(
     url_target,
     `export const CONFIG_META=${JSON.stringify(json)}`,
-    (err: any) => {
+    (err: unknown) => {
       err
-        ? log(LogLevel.ERROR, "生成csv的ts文件失败")
-        : log(LogLevel.NORMAL, "写入csv的ts文件成功");
+        ? log(LogLevel.Error, "生成csv的ts文件失败")
+        : log(LogLevel.Normal, "写入csv的ts文件成功");
     },
   );
 }
