64759aa1dfefcd5ce78001cb78f94ff0ec1be2bc
fengyong <fkworld@foxmail.com>
2020-06-24 16:28:19 +0800

    refactor: 完善时间模块

--- __test__/time.spec.ts
+++ __test__/time.spec.ts
@@ -1,50 +1,69 @@
-import * as time from "../src/time";
+import {
+  D_MS,
+  get_day,
+  H_MS,
+  M_MS,
+  S_MS,
+  to_ms,
+  to_time_format,
+  to_time_group,
+  to_time_string,
+} from "../src/time";
 
-test("D_MS", () => {
-  expect(time.D_MS).toBe(24 * 60 * 60 * 1000);
+describe("const", () => {
+  test("D_MS", () => {
+    expect(D_MS).toBe(24 * 60 * 60 * 1000);
+  });
+  test("H_MS", () => {
+    expect(H_MS).toBe(60 * 60 * 1000);
+  });
+  test("M_MS", () => {
+    expect(M_MS).toBe(60 * 1000);
+  });
+  test("S_MS", () => {
+    expect(S_MS).toBe(1000);
+  });
 });
 
-test("H_MS", () => {
-  expect(time.H_MS).toBe(60 * 60 * 1000);
+describe(to_ms.name, () => {
+  test("", () => {
+    expect(to_ms("1.5d")).toBe(129600000);
+    expect(to_ms("1.5")).toBeUndefined();
+  });
 });
 
-test("M_MS", () => {
-  expect(time.M_MS).toBe(60 * 1000);
+describe(to_time_group.name, () => {
+  test("", () => {
+    expect(to_time_group(88888)).toEqual({
+      h: 0,
+      h_fix: 0,
+      m: 1,
+      m_fix: 1,
+      s: 88,
+      s_fix: 28,
+      ms: 88888,
+      ms_fix: 888,
+    });
+  });
 });
 
-test("S_MS", () => {
-  expect(time.S_MS).toBe(1000);
+describe(to_time_format.name, () => {
+  test("", () => {
+    expect(to_time_format("1.5d")).toBe("36:00:00");
+    expect(to_time_format(129600000)).toBe("36:00:00");
+    expect(to_time_format(88888)).toBe("00:01:28");
+    expect(to_time_format(88888, false)).toBe("01:28");
+  });
 });
 
-test("get_day", () => {
-  expect(time.get_day(1590562765277)).toBe(18409);
+describe(to_time_string.name, () => {
+  test("", () => {
+    expect(to_time_string(1590562765277)).toBe("14:59:25 GMT+0800 (GMT+08:00)");
+  });
 });
 
-test("to_ms", () => {
-  expect(time.to_ms("1.5d")).toBe(129600000);
-  expect(time.to_ms("1.5")).toBeUndefined();
-  expect(time.to_show("1.5d")).toBe("36:00:00");
-});
-
-test("to_show", () => {
-  expect(time.to_show(129600000)).toBe("36:00:00");
-  expect(time.to_show(88888)).toBe("00:01:28");
-  expect(time.to_show(88888, false)).toBe("01:28");
-});
-
-test("to_timestring", () => {
-  expect(time.to_timestring(1590562765277)).toBe("14:59:25 GMT+0800 (GMT+08:00)");
-});
-
-test("to_group", () => {
-  expect(time.to_group(88888)).toEqual({
-    h: 0,
-    h_fix: 0,
-    m: 1,
-    m_fix: 1,
-    ms: 88888,
-    ms_fix: 888,
-    s: 88,
-    s_fix: 28,
+describe(get_day.name, () => {
+  test("", () => {
+    expect(get_day(1590562765277)).toBe(18409);
   });
 });
