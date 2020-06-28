9825553cc0c2a7f1adb3fd97505146ce6ad1ca13
fengyong <fkworld@foxmail.com>
2020-06-28 22:50:02 +0800

    chore: 删除api-extractor，暂时使用rollup-plugin-dts插件

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,19 @@
 //@ts-check
+
 import json from "@rollup/plugin-json";
 import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
 import { terser } from "rollup-plugin-terser";
 import { version } from "./package.json";
 
 /**
- * 创建2个配置
+ * 打包为2个js文件的配置
  * @param {boolean} is_terser 是否压缩代码
  * @returns {import("rollup").RollupOptions}
  */
 function create_config(is_terser) {
   return {
     input: "./src/index.ts",
-    plugins: [
-      // 从package.json中获取项目信息
-      json(),
-      // 用于打包typescript
-      typescript(),
-      is_terser ? terser() : undefined,
-    ],
     output: {
       // 输出文件
       file: `./dist/fy-${version}.${is_terser ? "min" : "dev"}.js`,
@@ -29,7 +24,32 @@ function create_config(is_terser) {
       // 文件标题
       banner: `/*! fy-${version} https://github.com/fkworld/cocos-game-framework */`,
     },
+    plugins: [
+      // 从package.json中获取项目信息
+      json(),
+      // 用于打包typescript
+      typescript(),
+      // 用于压缩js代码
+      is_terser ? terser() : undefined,
+    ],
   };
 }
 
-export default [create_config(false), create_config(true)];
+/**
+ * 打包声明文件dts的配置
+ * - 之前需要先生成声明文件，参考命令：npx tsc --declaration --emitDeclarationOnly --declarationDir ./dist
+ * @returns {import("rollup").RollupOptions}
+ */
+function create_config_dts_bundle() {
+  return {
+    input: "./dist/src/index.d.ts",
+    output: {
+      file: `./dist/fy-${version}.d.ts`,
+      banner: `/*! fy-${version} https://github.com/fkworld/cocos-game-framework */`,
+      footer: "export as namespace fy;",
+    },
+    plugins: [dts()],
+  };
+}
+
+export default [create_config_dts_bundle(), create_config(false), create_config(true)];
